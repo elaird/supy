aa67fc0af46f51bf6dc4e2bd2696a2d6a0e08544
elaird <elaird>
2010-06-12 14:55:17 +0000

    pas samples

--- samples.py
+++ samples.py
@@ -3,8 +3,7 @@ def getCommandOutput2(command):
     child = os.popen(command)
     data = child.read()
     err = child.close()
-    if err:
-        raise RuntimeError, '%s failed w/ exit code %d' % (command, err)
+    #if err: raise RuntimeError, '%s failed w/ exit code %d' % (command, err)
     return data
                         
 class sampleDictionaryHolder :
@@ -27,6 +26,347 @@ class sampleDictionaryHolder :
             if member[:len(specialPrefix)]!=specialPrefix : continue
             getattr(self,member)()
 
+    def add_met_pas_skims(self) :
+        baseDir="/vols/cms02/elaird1/03_skims/cleaned/"
+
+        subDirs=[
+            "JetMETTau.Run2010A-May27thReReco_v1.RECO",
+            "MinimumBias.Commissioning10-SD_JetMETTau-v9.RECO",
+            "MinimumBias.Commissioning10-May6thPDSkim2_SD_JetMETTau-v1.RECO",
+            "QCD_Pt-15_7TeV-pythia8.Spring10-START3X_V26B-v1.GEN-SIM-RECO",
+            ]
+
+        furtherSubDirs=["leading_uncorr_ak5CaloJet.gt.40"]
+        
+        for subDir in subDirs :
+            self.fileListDict[subDir+"_cleanEvent"]=getCommandOutput2("ls "+baseDir+"/"+subDir+"/*.root").split()
+            for furtherSubDir in furtherSubDirs :
+                self.fileListDict[subDir+"."+furtherSubDir]=getCommandOutput2("ls "+baseDir+"/"+subDir+"/"+furtherSubDir+"/*.root").split()
+
+    def add_Burt1(self) :
+        prefix="dcap://gfe02.hep.ph.ic.ac.uk:22128"
+        dir="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/bbetchar//ICF/automated/2010_06_08_03_27_37/"
+
+        files=[
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_2_2.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_22_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_10_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_4_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_32_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_15_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_5_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_29_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_14_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_19_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_28_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_30_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_6_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_23_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_20_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_26_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_16_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_31_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_17_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_25_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_12_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_21_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_27_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_7_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_13_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_8_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_11_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_1_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_3_1.root",
+            prefix+"/"+dir+"/"+"SusyCAF_Tree_9_1.root",
+            ]
+
+        self.fileListDict["JetMETTau.Run2010A-May27thReReco_v1.RECO"]=files
+        return
+    
+    def add_Burt2(self) :
+        prefix="dcap://gfe02.hep.ph.ic.ac.uk:22128"
+        dir="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/bbetchar//ICF/automated/2010_06_08_04_00_40/"
+
+        files=[
+            prefix+"/"+dir+"/SusyCAF_Tree_18_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_8_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_6_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_27_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_40_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_15_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_45_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_28_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_24_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_2_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_55_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_26_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_14_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_21_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_65_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_61_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_20_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_33_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_68_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_31_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_32_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_9_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_30_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_44_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_4_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_66_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_51_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_13_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_10_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_11_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_23_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_60_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_12_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_35_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_56_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_52_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_25_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_54_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_69_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_38_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_53_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_22_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_36_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_49_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_1_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_58_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_67_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_59_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_3_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_37_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_34_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_39_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_19_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_41_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_48_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_47_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_64_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_7_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_43_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_17_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_62_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_63_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_5_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_46_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_50_1.root",
+            ]
+
+        self.fileListDict["MinimumBias.Commissioning10-May6thPDSkim2_SD_JetMETTau-v1.RECO"]=files
+        return
+
+    def add_Burt3(self) :
+        prefix="dcap://gfe02.hep.ph.ic.ac.uk:22128"
+        dir="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/bbetchar//ICF/automated/2010_06_08_03_44_12/"
+
+        files=[
+            prefix+"/"+dir+"/SusyCAF_Tree_9_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_58_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_31_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_38_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_20_1.root",
+            #prefix+"/"+dir+"/SusyCAF_Tree_10_1.root",#not finished
+            prefix+"/"+dir+"/SusyCAF_Tree_151_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_51_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_1_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_14_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_99_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_2_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_37_1.root",
+            #prefix+"/"+dir+"/SusyCAF_Tree_28_1.root",#not finished
+            prefix+"/"+dir+"/SusyCAF_Tree_154_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_8_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_30_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_13_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_95_1.root",
+            #prefix+"/"+dir+"/SusyCAF_Tree_26_1.root",#not finished
+            prefix+"/"+dir+"/SusyCAF_Tree_29_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_132_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_74_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_15_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_16_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_105_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_7_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_72_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_45_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_62_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_68_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_24_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_130_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_70_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_60_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_55_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_76_1.root",
+            #prefix+"/"+dir+"/SusyCAF_Tree_56_2.root",#broken
+            prefix+"/"+dir+"/SusyCAF_Tree_103_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_104_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_102_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_25_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_5_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_150_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_67_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_93_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_27_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_42_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_18_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_86_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_23_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_43_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_96_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_22_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_82_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_40_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_89_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_50_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_144_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_64_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_153_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_47_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_146_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_21_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_98_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_110_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_11_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_129_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_75_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_118_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_48_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_155_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_73_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_54_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_19_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_61_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_78_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_35_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_71_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_119_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_52_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_109_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_90_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_128_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_143_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_101_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_91_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_83_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_115_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_139_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_126_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_65_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_127_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_107_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_36_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_133_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_122_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_134_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_116_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_69_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_131_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_121_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_100_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_84_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_87_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_148_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_80_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_125_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_85_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_152_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_59_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_124_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_156_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_113_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_112_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_111_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_123_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_138_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_120_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_108_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_46_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_147_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_117_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_17_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_142_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_44_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_136_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_141_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_106_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_145_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_66_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_137_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_114_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_140_1.root",
+            ]
+        
+        self.fileListDict["MinimumBias.Commissioning10-SD_JetMETTau-v9.RECO"]=files
+        return
+
+    def add_Burt4(self) :
+        prefix="dcap://gfe02.hep.ph.ic.ac.uk:22128"
+        dir="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/elaird//ICF/automated/2010_06_04_18_22_38/"
+
+        files=[
+            prefix+"/"+dir+"/SusyCAF_Tree_3_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_49_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_11_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_9_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_29_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_33_0.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_35_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_51_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_45_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_34_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_36_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_50_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_28_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_2_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_22_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_46_2.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_37_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_25_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_6_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_5_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_55_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_18_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_17_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_52_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_20_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_4_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_1_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_53_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_47_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_41_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_8_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_44_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_12_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_23_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_19_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_21_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_26_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_54_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_15_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_42_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_38_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_7_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_27_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_16_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_31_0.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_39_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_43_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_48_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_14_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_30_0.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_13_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_32_0.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_40_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_24_1.root",
+            prefix+"/"+dir+"/SusyCAF_Tree_56_1.root",
+            ]
+        
+        self.fileListDict["QCD_Pt-15_7TeV-pythia8.Spring10-START3X_V26B-v1.GEN-SIM-RECO"]=files
+        return
+    
     def add_Burt(self) :
         #self.fileListDict["Burt"]=getCommandOutput2('find /tmp/bbetchar/SusyCAF/2010_05_18_19_26_19/OUTPUT/ | grep .root').split()
         return
