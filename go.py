72db0b9705416d636378146f4511fb3d5e96d075
elaird <elaird>
2010-06-12 18:28:00 +0000

    partially working output merging

--- go.py
+++ go.py
@@ -16,11 +16,15 @@ name="example"
 #name="Ra1_NJet"
 #name="HcalTechTriggerCheck"
 #name="mSugraLook"
+#name="MetPasSkim"
+#name="MetPasSkim1"
+#name="MetPasSkim2"
+#name="MetPasSpeedTest"
 
 #choose the output directory
 outputDir="~"
 
-def makeSampleSpecs() :
+def makeSampleSpecs(name) :
     import samples,lists,analyses
     #build dictionaries of files and xs
     sampleHolder=samples.sampleDictionaryHolder()
@@ -41,7 +45,47 @@ def makeSampleSpecs() :
 
 def goFunc(x) :
     x.go()
+
+def mergeSplitOutput(looperList) :
+    import os
     
+    #make dictionary
+    parentDict={}
+    for iLooper in range(len(looperList)) :
+        looper=looperList[iLooper]
+        if looper.parentName!=None :
+            if looper.parentName in parentDict :
+                parentDict[looper.parentName].append(iLooper)
+            else :
+                parentDict[looper.parentName]=[iLooper]
+
+    #combine output
+    for parent in parentDict :
+        #print parent,parentDict[parent]
+        iSomeLooper=parentDict[parent][0]
+        someLooper=looperList[iSomeLooper]
+        outputPlotFileName=someLooper.outputPlotFileName.replace(someLooper.name,parent)
+        inFileList=""
+
+        for iLooper in parentDict[parent] :
+            thisLooper=looperList[iLooper]
+            plotFileName=thisLooper.outputPlotFileName
+            inFileList+=" "+plotFileName
+            for iStep in range(len(someLooper.steps)) :
+                print iStep,someLooper.steps[iStep].nTotal,thisLooper.steps[iStep].nTotal
+                someLooper.steps[iStep].nTotal+=thisLooper.steps[iStep].nTotal
+                someLooper.steps[iStep].nPass +=thisLooper.steps[iStep].nPass
+                someLooper.steps[iStep].nFail +=thisLooper.steps[iStep].nFail
+
+        print someLooper.hyphens
+        #print parent
+        #someLooper.printStats()
+                
+        cmd="hadd -f "+outputPlotFileName+inFileList+" | grep -v 'Source file' | grep -v 'Target path'"
+        #print "hadding",outputPlotFileName,"..."
+        os.system(cmd)
+        print someLooper.hyphens
+        
 def loopOverSamples() :
         looperList=[]
         for sample in sampleSpecs :
@@ -49,14 +93,16 @@ def loopOverSamples() :
 
         if ("multi" in sys.argv) :
             from multiprocessing import Pool
-            pool=Pool(processes=4)
+            pool=Pool(processes=6)
             #pool=Pool(processes=len(looperList))
             pool.map(goFunc,looperList)
         else :
             map(goFunc,looperList)
 
+        mergeSplitOutput(looperList)
+        
 #set up
-sampleSpecs=makeSampleSpecs()
+sampleSpecs=makeSampleSpecs(name)
 
 #loop over samples and make TFiles containing histograms
 if ("loop" in sys.argv and not "prof" in sys.argv) :
