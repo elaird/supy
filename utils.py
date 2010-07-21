75ae4b1dcfa235546708e312b0031531db0a8a91
elaird <elaird>
2010-07-21 16:41:16 +0000

    update prune function

--- utils.py
+++ utils.py
@@ -33,22 +33,42 @@ def pruneFileList(inList) :
     #make a dictionary of file versions
     splitString="_"
     stripString=".root"
-    d={}
+    versionDict={}
+    cruftDict={}
     for inFile in inList :
         fieldList=inFile.replace(stripString,"").split(splitString)
-        key=tuple(fieldList[:-1])
-        value=int(fieldList[-1])
-        if key not in d :
-            d[key]=[value]
+
+        #determine which field is the file version (newer versions of CRAB use also a random string)
+        versionIndex=-1
+        cruftString=""
+        try:
+            int(fieldList[-1])
+        except:
+            versionIndex=-2
+            cruftString="_"+fieldList[-1]
+
+        key=tuple(fieldList[:versionIndex])
+        value=int(fieldList[versionIndex])
+
+        if key not in versionDict :
+            versionDict[key]=[value]
+            cruftDict[key]=[cruftString]
         else :
-            d[key].append(value)
+            print "duplicate found and removed:",key
+            versionDict[key].append(value)
+            cruftDict[key].append(cruftString)
 
     #select the highest-numbered version of each file
     outList=[]
-    for key in d :
-        #print key,d[key]
+    for key in versionDict :
+        #print key,versionDict[key]
         fileName=splitString.join(key)
-        fileName+=splitString+str(max(d[key]))+stripString
+
+        maxVersion=max(versionDict[key])
+        index=versionDict[key].index(maxVersion)
+        cruftString=cruftDict[key][index]
+
+        fileName+=splitString+str(maxVersion)+cruftString+stripString
         outList.append(fileName)
     return outList
 #####################################
