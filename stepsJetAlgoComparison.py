421b3a756c3563d7b22d215784b867af7904e864
bbetchar <bbetchar>
2010-07-29 10:17:10 +0000

    complete transition to eventVars

--- stepsJetAlgoComparison.py
+++ stepsJetAlgoComparison.py
@@ -5,14 +5,10 @@ class leadingJetPtHistogrammer(analysisStep) :
     """leadingJetPtHistogrammer"""
 
     def __init__(self,jetCollection1,jetSuffix1,jetCollection2,jetSuffix2) :
-        self.jetCollection1=jetCollection1
-        self.jetSuffix1=jetSuffix1
-        self.jetCollection2=jetCollection2
-        self.jetSuffix2=jetSuffix2
-        self.neededBranches=[
-            self.jetCollection1+'CorrectedP4'+self.jetSuffix1,
-            self.jetCollection2+'CorrectedP4'+self.jetSuffix2,
-            ]
+        self.jetCollection1 = jetCollection1
+        self.jetCollection2 = jetCollection2
+        self.p4jets1 = jetCollection1+'CorrectedP4'+jetSuffix1,
+        self.p4jets2 = jetCollection2+'CorrectedP4'+jetSuffix2,
 
     def bookHistos(self) :
         self.leadingJetPtHisto=r.TH2D("ptLeadingComparison",
@@ -20,7 +16,7 @@ class leadingJetPtHistogrammer(analysisStep) :
                                       +";p_{T} (GeV) of leading "+self.jetCollection2+" jet"
                                       +";events / bin",20,0.0,50.0,20,0.0,50.0)
 
-    def uponAcceptance (self,chain,chainVars,extraVars) :
-        pt1=getattr(chainVars,self.jetCollection1+'CorrectedP4'+self.jetSuffix1)[0].pt()
-        pt2=getattr(chainVars,self.jetCollection2+'CorrectedP4'+self.jetSuffix2)[0].pt()
+    def uponAcceptance (self,evetVars,extraVars) :
+        pt1 = eventVars[self.p4jets1].at(0).pt()
+        pt2 = eventVars[self.p4jets1].at(0).pt()
         self.leadingJetPtHisto.Fill(pt1,pt2)
