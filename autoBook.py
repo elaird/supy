521a6533a4933316a342d205e49770e20eae60c8
elaird <elaird>
2010-07-30 22:26:06 +0000

    add Burts book

---
+++ autoBook.py
@@ -0,0 +1,29 @@
+import ROOT as r
+
+class autoBook(dict) :
+    def __init__(self,title="") :
+        self.__title = title
+        self.__directory = 0 if title=="" else r.TDirectory(title,title)
+        
+    def fill(self,x, name, N, low, up, w=1) :
+        if not name in self :
+            self[name] = \
+                r.TH1D(    name,"",N,low,up) if type(x)!=tuple else \
+                r.TProfile(name,"",N,low,up) if type(N)!=tuple else \
+                r.TH2D(name,"",
+                       N[0],low[0],up[0],
+                       N[1],low[1],up[1]) if len(N)==2 else \
+                r.TH3D(name,"",
+                       N[0],low[0],up[0],
+                       N[1],low[1],up[1],
+                       N[2],low[2],up[2])
+            
+            self[name].SetDirectory(self.__directory);
+            if not self[name].GetSumw2N() : self[name].Sumw2()
+
+        if    type(x)!=tuple : self[name].Fill(x,w)
+        elif  type(N)!=tuple : self[name].Fill(x[0],x[1],w)
+        elif  len(N)==2      : self[name].Fill(x[0],x[1],w)
+        else                 : self[name].Fill(x[0],x[1],x[2],w)
+        
+
