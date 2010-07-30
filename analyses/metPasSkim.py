dcf4b9a06004eaefcbfdcbcf74444b7c5d8078ea
elaird <elaird>
2010-07-30 21:24:35 +0000

    re-organize

--- analyses/metPasSkim.py
+++ analyses/metPasSkim.py
@@ -10,15 +10,17 @@ a.addSampleSpec(sampleName="JetMETTau.Run2010A-Jun14thReReco_v2.RECO.Bryn",
                 listOfFileNames=utils.fileListFromSrmLs(location="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/bm409//ICF/automated/2010_07_20_16_52_06/",nMaxFiles=-1),
                 listName="metPasFilter",
                 isMc=False,
+                lumi=0.012,                
                 nEvents=-1)
 
-#a.addSampleSpec(sampleName="JetMETTau.Run2010A-Jul16thReReco-v1.RECO.Bryn",
-#                listOfFileNames=utils.fileListFromSrmLs(location="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/bm409//ICF/automated/2010_07_20_17_20_35/",
-#                                                        itemsToSkip=["123_5_062","79_1_034"],
-#                                                        nMaxFiles=-1),
-#                listName="metPasFilter",
-#                isMc=False,
-#                nEvents=-1)
+a.addSampleSpec(sampleName="JetMETTau.Run2010A-Jul16thReReco-v1.RECO.Bryn",
+                listOfFileNames=utils.fileListFromSrmLs(location="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/bm409//ICF/automated/2010_07_20_17_20_35/",
+                                                        itemsToSkip=["123_5_062","79_1_034"],
+                                                        nMaxFiles=-1),
+                listName="metPasFilter",
+                isMc=False,
+                lumi=0.120,
+                nEvents=-1)
 
 #a.addSampleSpec(sampleName="JetMETTau.Run2010A-Jul6thReReco_v1.RECO_cleanEvent",
 #                listOfFileNames=utils.getCommandOutput2("ls /vols/cms02/elaird1/05_skims/JetMETTau.Run2010A-Jul6thReReco_v1.RECO/*.root").split("\n")[:1],
@@ -26,9 +28,6 @@ a.addSampleSpec(sampleName="JetMETTau.Run2010A-Jun14thReReco_v2.RECO.Bryn",
 #                isMc=False,
 #                nEvents=-1)
 
-a.go(loop=True,
-     plot=False,
-     profile=False,
-     nCores=6,
-     splitJobsByInputFile=True
-     )
+a.splitJobsByInputFile()
+a.loop(profile=False,nCores=6)
+#a.plot()
