b21ca5d31b7b4b9065fc00fe22686fc40b18e2dd
bbetchar <bbetchar>
2010-08-02 18:01:25 +0000

    fixed case where calculable with no arg init was not included in zerArgs()

--- calculables.py
+++ calculables.py
@@ -1,5 +1,5 @@
 from wrappedChain import *
-from inspect import isclass,ismodule
+from inspect import isclass,ismodule,getargspec
 import calculablesJet
 import calculablesOther
 
@@ -14,7 +14,9 @@ def zeroArgs() :
             calc = eval("%s.%s"%(moduleName,className))
             if not isclass(calc) : continue
             if not issubclass(calc, wrappedChain.calculable) : continue
-            try: eval('%s.%s.__init__.im_func'%(moduleName,className))
+            try:
+                args = len(getargspec(eval("%s.%s.__init__.im_func"%(moduleName,className)))[0])
+                if args < 2 :
+                    zeroArg.append(calc())
             except: zeroArg.append(calc())
     return zeroArg
-
