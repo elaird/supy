21ac3ba842e052122f2449515818ebc78ac7ca1a
elaird <elaird>
2010-08-02 23:31:21 +0000

    book

--- stepsJetAlgoComparison.py
+++ stepsJetAlgoComparison.py
@@ -10,13 +10,14 @@ class leadingJetPtHistogrammer(analysisStep) :
         self.p4jets1 = jetCollection1+'CorrectedP4'+jetSuffix1,
         self.p4jets2 = jetCollection2+'CorrectedP4'+jetSuffix2,
 
-    def bookHistos(self) :
-        self.leadingJetPtHisto=r.TH2D("ptLeadingComparison",
-                                      ";p_{T} (GeV) of leading "+self.jetCollection1+" jet"
-                                      +";p_{T} (GeV) of leading "+self.jetCollection2+" jet"
-                                      +";events / bin",20,0.0,50.0,20,0.0,50.0)
-
-    def uponAcceptance (self,evetVars) :
+    def uponAcceptance (self,eventVars) :
         pt1 = eventVars[self.p4jets1].at(0).pt()
         pt2 = eventVars[self.p4jets1].at(0).pt()
-        self.leadingJetPtHisto.Fill(pt1,pt2)
+        self.book(eventVars).fill( (pt1,pt2),
+                                   "ptLeadingComparison",
+                                   (20,20),
+                                   (0.0,0.0),
+                                   (50.0,50.0),
+                                   ";p_{T} (GeV) of leading "+self.jetCollection1+" jet"+ \
+                                   ";p_{T} (GeV) of leading "+self.jetCollection2+" jet"+ \
+                                   +";events / bin")
