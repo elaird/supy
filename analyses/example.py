e0dc126a467c833195c586382c93f687e423fb4c
elaird <elaird>
2010-08-04 12:07:29 +0000

    update

--- analyses/example.py
+++ analyses/example.py
@@ -18,7 +18,6 @@ def makeSteps() :
         steps.jetPtSelector(jetCollection,jetSuffix,jetPtThreshold,0),#leading corrected jet
         steps.jetPtSelector(jetCollection,jetSuffix,jetPtThreshold,1),#next corrected jet
         #steps.jetPtVetoer(jetCollection,jetSuffix,jetPtThreshold,2),#next corrected jet
-
         steps.minNCleanJetEventFilter(jetCollection,jetSuffix,2),
         steps.maxNOtherJetEventFilter(jetCollection,jetSuffix,0),
 
@@ -26,21 +25,20 @@ def makeSteps() :
         steps.cleanJetHtMhtHistogrammer(jetCollection,jetSuffix),
         #steps.variableGreaterFilter(25.0,jetCollection+"SumPt"+jetSuffix),
 
-        steps.cleanDiJetAlphaProducer(jetCollection,jetSuffix),
         steps.alphaHistogrammer(jetCollection,jetSuffix),
-
-        #steps.skimmer("/tmp/"),
+        #steps.skimmer("/tmp/%s/"%os.environ["USER"]),
         ]
     return outSteps
 
 def makeCalculables() :
     jettypes = ["ak5Jet","ak5JetJPT","ak5JetPF"]
     listOfCalculables = calculables.zeroArgs()
-    listOfCalculables += [ calculablesJet.indices( collection = col, suffix = "Pat", ptMin = 20.0, etaMax = 3.0, flagName = "JetIDloose") for col in jettypes]
-    listOfCalculables += [ calculablesJet.sumPt( collection = col, suffix = "Pat")                                                        for col in jettypes]
-    listOfCalculables += [ calculablesJet.sumP4( collection = col, suffix = "Pat")                                                        for col in jettypes]
+    listOfCalculables += [ calculablesJet.indices(        collection = col, suffix = "Pat", ptMin = 20.0, etaMax = 3.0, flagName = "JetIDloose") for col in jettypes]
+    listOfCalculables += [ calculablesJet.sumPt(          collection = col, suffix = "Pat") for col in jettypes ]
+    listOfCalculables += [ calculablesJet.sumP4(          collection = col, suffix = "Pat") for col in jettypes ]
     listOfCalculables += [ calculablesJet.deltaPseudoJet( collection = col, suffix = "Pat") for col in jettypes ]
-    listOfCalculables += [ calculablesJet.alphaT( collection = col, suffix = "Pat") for col in jettypes ]
+    listOfCalculables += [ calculablesJet.alphaT(         collection = col, suffix = "Pat") for col in jettypes ]
+    listOfCalculables += [ calculablesJet.diJetAlpha(     collection = col, suffix = "Pat") for col in jettypes ]
     return listOfCalculables
 
 a=analysis.analysis(name="example",
@@ -52,13 +50,13 @@ a=analysis.analysis(name="example",
 a.addSample(sampleName="Example_Skimmed_900_GeV_Data", nEvents = -1, lumi = 1.0e-5, #/pb
             listOfFileNames=["/afs/cern.ch/user/e/elaird/public/susypvt/framework_take3/skimmed_900_GeV_Data.root"] )
 
-a.addSample(sampleName="Example_Skimmed_900_GeV_MC", nEvents = 100, xs = 1.0e3, #pb
+a.addSample(sampleName="Example_Skimmed_900_GeV_MC", nEvents = -1, xs = 1.0e8, #pb
             listOfFileNames=["/afs/cern.ch/user/e/elaird/public/susypvt/framework_take3/skimmed_900_GeV_MC.root"] )
 
 
 #loop over events and make root files containing histograms
-a.loop(profile=False,              #profile the code
-       nCores=1,                   #use multiple cores to process input files in parallel
+a.loop(profile=False,   #profile the code
+       nCores=1,        #use multiple cores to process input files in parallel
        )
 
 #make a pdf file with plots from the histograms created above
