ab93c2f378acfdfcb3e1215a510144e6a1fe8eb7
elaird <elaird>
2010-08-10 12:50:10 +0000

    fix dcap in env script

--- supyenv.sh
+++ supyenv.sh
@@ -1,1 +1,11 @@
+if [ $HOSTNAME == "lx05.hep.ph.ic.ac.uk" ]
+    then
+    export LD_LIBRARY_PATH=/vols/cms/grid/dcap:/vols/grid/glite/ui/current/d-cache/dcap/lib:$LD_LIBRARY_PATH
+fi
+
+if [ $HOSTNAME == "lx06.hep.ph.ic.ac.uk" ]
+    then
+    export LD_LIBRARY_PATH=/vols/cms/grid/dcap:/vols/grid/glite/ui/current/d-cache/dcap/lib:$LD_LIBRARY_PATH
+fi
+
 export PYTHONPATH=${PYTHONPATH}:${PWD}
