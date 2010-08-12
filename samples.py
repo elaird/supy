9474da193c63c60ebe94cb98f62ddd4ccf22d7bc
bbetchar <bbetchar>
2010-08-12 22:44:50 +0000

    samply stuff

--- samples.py
+++ samples.py
@@ -6,29 +6,35 @@ def specify(name = None, nFilesMax = -1, nEventsMax = -1, color = r.kBlack, mark
     
 class SampleHolder(dict) :
     sample = collections.namedtuple("sample", "filesCommand xs lumi ptHatMin")
+    overlapping = collections.namedtuple("overlappingSample", "samples useRejectionMethod")
 
-    def __init__(self) : self.overlappingSamples = []
+    def __init__(self) :
+        self.overlappingSamples = []
+    
+    def update(other) :
+
+        assert type(other) is type(self), "%s is not a SampleHolder" % str(type(other))
+        for key in other : assert key not in self, "%s already specified" % key
+
+        self.update(other)
+        map(lambda t: self.adjustOverlappingSamples(*t), other.overlappingSamples)
 
     def add(self, name, filesCommand = None, xs = None, lumi = None, ptHatMin = None) :
 
-        if lumi and (xs or ptHatMin) : raise Exception("Overspecified sample: %s"%name)
-        if not (lumi or xs) :          raise Exception("Underspecified sample: %s"%name)
+        assert lumi is None or xs is None,   "Underspecified sample: %s"%name
+        assert lumi ^ (xs or ptHatMin), "Overspecified sample: %s"%name
 
         self[name] = self.sample(filesCommand, xs, lumi, ptHatMin)
 
     def adjustOverlappingSamples( self, listOfSamples, useRejectionMethod = True ) :
-        if len(listOfSamples) is not len(set(listOfSamples)) :
-            raise Exception("Non unique overlapping samples",listOfSamples)
+        assert len(listOfSamples) == len(set(listOfSamples)), "Duplicate samples in: %s"%str(listOfSamples)
 
         for s in listOfSamples :
-            if s not in self :
-                raise Exception("unknown sample",s)
-
-        for s in listOfSamples :
+            assert s in self, "Unknown sample"%s
+            assert self[s].ptHatMin, "ptHatMin unspecified for sample: %s"%s
             for otherOverlappingSamples in self.overlappingSamples :
-                if s in otherOverlappingSamples[0] :
-                    raise Exception("sample already grouped with others",s)
+                assert s not in otherOverlappingSamples[0], "Sample in another unbinned group: %s"%s
 
-        self.overlappingSamples.append( (listOfSamples, useRejectionMethod ) )
+        self.overlappingSamples.append( overlapping(listOfSamples, useRejectionMethod ) )
 
 from samplesMC import *
