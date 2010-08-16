16f099e0e412b8cbb23ac10c7146ee32a0df9020
bbetchar <bbetchar>
2010-08-16 20:59:22 +0000

    Fix off by one error nEntries comparison

--- wrappedChain.py
+++ wrappedChain.py
@@ -30,7 +30,7 @@ class wrappedChain(dict) :
         """Generate the access dictionary (self) for each entry in TTree."""
         if not self.__chain: return
         chain = self.__chain
-        nEntries = (lambda x,y : min(x,y) if x>=0 else y)( nEntries, chain.GetEntries() )
+        nEntriesMinusOne = (lambda x,y : min(x,y) if x>=0 else y)( nEntries, chain.GetEntries() ) - 1
         iTreeFirstEntry = 0
         
         for nTree in range(chain.GetNtrees()) :
@@ -42,7 +42,7 @@ class wrappedChain(dict) :
 
             for iTreeEntry in range( nTreeEntries )  :
                 self.entry = iTreeFirstEntry + iTreeEntry
-                if nEntries <= self.entry : return
+                if nEntriesMinusOne <= self.entry : return
                 self.__localEntry = iTreeEntry
                 for node in self.__activeNodeList : node.updated = False
                 yield self
