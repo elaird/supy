99dd1d90e86f13c025485f37f41dc2fc86273433
bbetchar <bbetchar>
2010-08-17 00:22:20 +0000

    put newly booked histos directly into the correct directory

--- autoBook.py
+++ autoBook.py
@@ -8,14 +8,13 @@ class autoBook(dict) :
         
     def fill(self,x, name, N, low, up, w=1, title="") :
         if not name in self :
+            self.__directory.cd()
             self.fillOrder.append(name)
             self[name] = \
                        r.TH1D( name, title, N, low, up)                            if type(x)!=tuple else \
                        r.TProfile( name, title, N, low, up)                        if type(N)!=tuple else \
                        r.TH2D( name,title, N[0],low[0],up[0],  N[1],low[1],up[1])  if len(N)==2      else \
                        r.TH3D( name,title, N[0],low[0],up[0],  N[1],low[1],up[1],  N[2],low[2],up[2])
-            
-            self[name].SetDirectory(self.__directory)
             if not self[name].GetSumw2N() : self[name].Sumw2()
             
         if    type(x)!=tuple : self[name].Fill(x,w)
