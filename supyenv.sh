ce4a4ab04b693bc3ee00fff06feb2bcaa2463ac9
elaird <elaird>
2010-08-18 22:50:15 +0000

    module path set up in supy

--- supyenv.sh
+++ supyenv.sh
@@ -8,4 +8,3 @@ if [ $HOSTNAME == "lx06.hep.ph.ic.ac.uk" ]
     export LD_LIBRARY_PATH=/vols/cms/grid/dcap:/vols/grid/glite/ui/current/d-cache/dcap/lib:$LD_LIBRARY_PATH
 fi
 
-export PYTHONPATH=${PYTHONPATH}:${PWD}:${PWD}/"analyses"
