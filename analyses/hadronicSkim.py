cfab8ef25d1ef79de4284d1d2e1518e97944080b
elaird <elaird>
2010-08-18 23:20:04 +0000

    re-structure

--- analyses/hadronicSkim.py
+++ analyses/hadronicSkim.py
@@ -2,42 +2,39 @@
 
 import os,analysis,utils,calculables,steps,samples
 
-def makeSteps() :
-    jetAlgoList=[("ak5Jet"+jetType,"Pat") for jetType in ["","PF","JPT"]]
-    stepList=[ steps.progressPrinter(2,300),
-               steps.hltFilter("HLT_Jet50U"),
-               steps.leadingUnCorrJetPtSelector(jetAlgoList,100.0),
-               steps.techBitFilter([0],True),
-               steps.physicsDeclared(),
-               steps.vertexRequirementFilter(5.0,24.0),
-               steps.monsterEventFilter(10,0.25),
-               steps.skimmer("/vols/cms02/%s/"%os.environ["USER"]),
-               ]
-    return stepList
+class hadronicSkim(analysis.analysis) :
+    def outputDirectory(self) :
+        return "/vols/cms02/%s/tmp/"%os.environ["USER"]
 
-def makeCalculables() :
-    return calculables.zeroArgs()
+    def listOfSteps(self) :
+        jetAlgoList=[("ak5Jet"+jetType,"Pat") for jetType in ["","PF","JPT"]]
+        stepList=[ steps.progressPrinter(2,300),
+                   steps.hltFilter("HLT_Jet50U"),
+                   steps.leadingUnCorrJetPtSelector(jetAlgoList,100.0),
+                   steps.techBitFilter([0],True),
+                   steps.physicsDeclared(),
+                   steps.vertexRequirementFilter(5.0,24.0),
+                   steps.monsterEventFilter(10,0.25),
+                   steps.skimmer("/vols/cms02/%s/"%os.environ["USER"]),
+                   ]
+        return stepList
 
-def makeSamples() :
-    from samples import specify
-    return [#specify(name = "MinimumBias.Commissioning10-SD_JetMETTau-Jun14thSkim_v1.RECO.Bryn" ),
-            #specify(name = "JetMETTau.Run2010A-Jun14thReReco_v2.RECO.Bryn"                     ),
-            #specify(name = "JetMETTau.Run2010A-Jul16thReReco-v1.RECO.Bryn"                     ),
-            #specify(name = "JetMETTau.Run2010A-PromptReco-v4.RECO.Bryn"                        ),
-            #specify(name = "JetMETTau.Run2010A-PromptReco-v4.RECO.Loukas"                      ),
-            #specify(name = "JetMET.Run2010A-PromptReco-v4.RECO.Loukas"                         ),
-            specify(name = "JetMET.Run2010A-PromptReco-v4.RECO.Martyn"                         ),
-            #specify(name = "z_inv_mg"    ),
-            #specify(name = "z_jets_mg"   ),
-            #specify(name = "w_jets_mg"   ),
-            ]
-    
-a=analysis.analysis(name = "hadronicSkim",
-                    outputDir = "/vols/cms02/%s/tmp/"%os.environ["USER"],
-                    listOfSteps = makeSteps(),
-                    listOfCalculables = makeCalculables(),
-                    listOfSamples = makeSamples(),
-                    listOfSampleDictionaries = [samples.jetmet,samples.mc]
-                    )
+    def listOfCalculables(self) :
+        return calculables.zeroArgs()
 
-a.loop( nCores = 6 )
+    def listOfSamples(self) :
+        from samples import specify
+        return [#specify(name = "MinimumBias.Commissioning10-SD_JetMETTau-Jun14thSkim_v1.RECO.Bryn" ),
+                #specify(name = "JetMETTau.Run2010A-Jun14thReReco_v2.RECO.Bryn"                     ),
+                #specify(name = "JetMETTau.Run2010A-Jul16thReReco-v1.RECO.Bryn"                     ),
+                #specify(name = "JetMETTau.Run2010A-PromptReco-v4.RECO.Bryn"                        ),
+                #specify(name = "JetMETTau.Run2010A-PromptReco-v4.RECO.Loukas"                      ),
+                #specify(name = "JetMET.Run2010A-PromptReco-v4.RECO.Loukas"                         ),
+                specify(name = "JetMET.Run2010A-PromptReco-v4.RECO.Martyn"                         ),
+                #specify(name = "z_inv_mg"    ),
+                #specify(name = "z_jets_mg"   ),
+                #specify(name = "w_jets_mg"   ),
+                ]
+
+    def listOfSampleDictionaries(self) :
+        return [samples.jetmet,samples.mc]
