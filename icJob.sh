aff2fbfb35d886f05f4fe17f6c991d5175fc64b8
elaird <elaird>
2010-08-19 21:09:53 +0000

    more env

--- icJob.sh
+++ icJob.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export X509_USER_PROXY=/home/hep/$USER/.MyProxy
 USERTMP="/vols/cms02/$USER/tmp"
 CMSSW=/vols/sl5_exp_software/cms/slc5_ia32_gcc434/cms/cmssw/CMSSW_3_7_0/src
 mkdir -p $USERTMP >& /dev/null
