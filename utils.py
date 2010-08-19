294883e039e02ecba72dc1a72f623191b311cf79
bbetchar <bbetchar>
2010-08-19 00:48:26 +0000

    roundstring

--- utils.py
+++ utils.py
@@ -276,3 +276,26 @@ def ratioHistogram(num,den) :
         
     return ratio
 #####################################
+def roundString(val,err,width=None) :
+    err_digit = int(math.floor(math.log(abs(err))/math.log(10))) if err else 0
+    val_digit = int(math.floor(math.log(abs(val))/math.log(10))) if val else 0
+    dsp_digit = max(err_digit,val_digit)
+    sci = val_digit<-1 or err_digit>0
+
+    precision = val_digit-err_digit if sci else -err_digit
+
+    display_val = val/pow(10.,dsp_digit) if sci else val
+    display_err = str(int(round(err/pow(10,err_digit))))
+
+    while True:
+        display_sci = ("e%+d"%dsp_digit) if sci else ""
+        returnVal = "%.*f(%s)%s"%(precision,display_val,display_err,display_sci)
+        if (not width) or len(returnVal) <= width or precision < 1: break
+        else:
+            display_err = "-"
+            precision-=1
+            if not precision :
+                display_val*=10
+                dsp_digit-=1
+    return returnVal
+########################
