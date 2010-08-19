a8b3aab10e55573436e36a67f542e5fa249e51a0
elaird <elaird>
2010-08-19 01:15:24 +0000

    batch updates

--- icJob.sh
+++ icJob.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+USERTMP="/vols/cms02/$USER/tmp"
+CMSSW=/vols/sl5_exp_software/cms/slc5_ia32_gcc434/cms/cmssw/CMSSW_3_7_0/src
+mkdir -p $USERTMP >& /dev/null
 . /vols/cms/grid/setup.sh
-cd ~elaird1/03_SusyCAFmacro/CMSSW_3_6_1/src/ && eval `scram runtime -sh` && cd - >& /dev/null
-cd ~elaird1/supy_dev2
+cd $CMSSW && eval `scram runtime -sh` && cd - >& /dev/null
 export LD_LIBRARY_PATH=/vols/cms/grid/dcap:/vols/grid/glite/ui/current/d-cache/dcap/lib:$LD_LIBRARY_PATH
