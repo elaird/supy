9a32b89edea66a4b27fe677bce4078e925279315
elaird <elaird>
2010-08-20 13:59:17 +0000

    batch system handles output

--- icJob.sh
+++ icJob.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export X509_USER_PROXY=/home/hep/$USER/.MyProxy
-USERTMP="/vols/cms02/$USER/tmp"
+#USERTMP="/vols/cms02/$USER/tmp"
 CMSSW=/vols/sl5_exp_software/cms/slc5_ia32_gcc434/cms/cmssw/CMSSW_3_7_0/src
-mkdir -p $USERTMP >& /dev/null
+#mkdir -p $USERTMP >& /dev/null
 . /vols/cms/grid/setup.sh
 cd $CMSSW && eval `scram runtime -sh` && cd - >& /dev/null
 export LD_LIBRARY_PATH=/vols/cms/grid/dcap:/vols/grid/glite/ui/current/d-cache/dcap/lib:$LD_LIBRARY_PATH
