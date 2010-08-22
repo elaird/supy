f4cd3b8e09102e2a357dcbdb248f237edcbaef2b
bbetchar <bbetchar>
2010-08-22 22:44:48 +0000

    update

--- analyses/photonSelection.py
+++ analyses/photonSelection.py
@@ -29,16 +29,16 @@ class photonSelection(analysis.analysis) :
                 [ calculables.xcJet( _jet,  gamma = _photon, gammaDR = 0.5, 
                                      muon = _muon, muonDR = 0.5),
                   calculables.jetIndices( _jet, ptMin = 20.0, etaMax = 3.0, flagName = params["jetId"]),
-                 calculables.photonIndicesPat(  ptMin = 30, flagName="photonIDLoosePat"),
+                  calculables.photonIndicesPat(  ptMin = 30, flagName="photonIDLoosePat"),
                   calculables.muonIndices( _muon, ptMin = 20.0, combinedRelIsoMax = 0.15),
-                  calculables.genIndices( label = "Z",      pdgs = [23]),
-                  calculables.genIndices( label = "Photon", pdgs = [22]),
+                  calculables.genIndices( label = "GammaZ", pdgs = [22,23]),
                   ]
         return calcs
 
     def listOfSteps(self,params) :
         _jet = params["objects"]["jet"]
         _muon = params["objects"]["muon"]
+        _muon = params["objects"]["photon"]
         stepList = [
             steps.progressPrinter(),
 
@@ -53,14 +53,15 @@ class photonSelection(analysis.analysis) :
         
             steps.multiplicityFilter("%sIndices%s"%_jet, nMin = params["nJetsMinMax"][0], nMax = params["nJetsMinMax"][1]),
             steps.multiplicityFilter("%sIndicesOther%s"%_jet, nMax = 0),
-            steps.multiplicityFilter("%sIndices%s"%_muon, nMax = 0),
             steps.variableGreaterFilter(350.0,"%sSumPt%s"%_jet),
+            #steps.multiplicityFilter("%sIndices%s"%_photon, nMax = 0),
+            steps.multiplicityFilter("%sIndices%s"%_muon, nMax = 0),
+
+            steps.alphaTetaDependence(_jet),
             steps.variableGreaterFilter(0.55,"%sAlphaT%s"%_jet),
+            steps.alphaTetaDependence(_jet)
 
             # signal distributions for {g,Z}x{forward,mid,central}
-
-            #steps.photonSelectionHistogrammer( matchDeltaRMax = 0.1),
-            steps.alphaHistogrammer(_jet),
             ]
         return stepList
         
@@ -70,24 +71,24 @@ class photonSelection(analysis.analysis) :
 
     def listOfSamples(self) :
         from samples import specify
-        return [  specify(name = "JetMET_skim",           nFilesMax = 1, color = r.kBlack   , markerStyle = 20),
+        return [  #specify(name = "JetMET_skim",           nFilesMax = 1, color = r.kBlack   , markerStyle = 20),
                 ##specify(name = "qcd_mg_ht_250_500_old", nFilesMax = 1, color = r.kBlue    ),
                 ##specify(name = "qcd_py_pt30",           nFilesMax = 1, color = r.kBlue    ),
-                  specify(name = "qcd_py_pt80",           nFilesMax = 1, color = r.kBlue    ),
-                  specify(name = "qcd_py_pt170",          nFilesMax = 1, color = r.kBlue    ),
-                  specify(name = "qcd_py_pt300",          nFilesMax = 1, color = r.kBlue    ),
+                  #specify(name = "qcd_py_pt80",           nFilesMax = 1, color = r.kBlue    ),
+                  #specify(name = "qcd_py_pt170",          nFilesMax = 1, color = r.kBlue    ),
+                  #specify(name = "qcd_py_pt300",          nFilesMax = 1, color = r.kBlue    ),
                 ##specify(name = "qcd_py_pt470",          nFilesMax = 1, color = r.kBlue    ),
                 ##specify(name = "qcd_py_pt800",          nFilesMax = 1, color = r.kBlue    ),
                 ##specify(name = "qcd_py_pt1400",         nFilesMax = 1, color = r.kBlue    ),
-                  specify(name = "tt_tauola_mg",          nFilesMax = 1, color = r.kOrange  ),
+                  #specify(name = "tt_tauola_mg",          nFilesMax = 1, color = r.kOrange  ),
                   specify(name = "g_jets_mg_pt40_100",    nFilesMax = 1, color = r.kGreen   ),
                   specify(name = "g_jets_mg_pt100_200",   nFilesMax = 1, color = r.kGreen   ),
                   specify(name = "g_jets_mg_pt200",       nFilesMax = 1, color = r.kGreen   ),
                   specify(name = "z_inv_mg_skim",         nFilesMax = 1, color = r.kMagenta ),
-                  specify(name = "z_jets_mg_skim",        nFilesMax = 1, color = r.kYellow-3),
-                  specify(name = "w_jets_mg_skim",        nFilesMax = 1, color = 28         ),
-                  specify(name = "lm0",                   nFilesMax = 1, color = r.kRed     ),
-                  specify(name = "lm1",                   nFilesMax = 1, color = r.kRed+1   ),
+                  #specify(name = "z_jets_mg_skim",        nFilesMax = 1, color = r.kYellow-3),
+                  #specify(name = "w_jets_mg_skim",        nFilesMax = 1, color = 28         ),
+                  #specify(name = "lm0",                   nFilesMax = 1, color = r.kRed     ),
+                  #specify(name = "lm1",                   nFilesMax = 1, color = r.kRed+1   ),
                   ]
 
     def conclude(self) :
@@ -99,7 +100,7 @@ class photonSelection(analysis.analysis) :
             org.mergeSamples(targetSpec = {"name":"standard_model","color":r.kGreen+3},
                              sources = ["g_jets_mg","qcd_py","tt_tauola_mg","z_inv_mg_skim","z_jets_mg_skim","w_jets_mg_skim"], keepSources = True
                              )
-            org.scale()
+            org.scale(100)
 
             #plot
             pl = plotter.plotter(org,
