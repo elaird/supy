3973cad8e584e32ea040b663b5667d492977c5c7
bbetchar <bbetchar>
2010-08-22 21:01:15 +0000

    update

--- analyses/photonSelection.py
+++ analyses/photonSelection.py
@@ -1,93 +1,111 @@
 #!/usr/bin/env python
 
 import os
-import analysis,utils,steps,calculables,samples,plotter
+import analysis,steps,calculables,samples,organizer,plotter
 import ROOT as r
 
-jetTypes = [("ak5Jet","Pat"),("ak5JetJPT","Pat"),("ak5JetPF","Pat")]
+class photonSelection(analysis.analysis) :
+    def baseOutputDirectory(self) :
+        return "/vols/cms02/%s/tmp/"%os.environ["USER"]
 
-def makeSteps() :
-    jets = jetTypes[0]
-    xcjets = ("xc"+jets[0],jets[1])
-    
-    listOfSteps=[
-        steps.progressPrinter(),
+    def parameters(self) :
+        objects = {}
+        fields =                               ["jet",                 "met",             "muon",        "electron",        "photon",         "genjet","rechit"]
+        objects["caloAK5"] = dict(zip(fields, [("xcak5Jet","Pat"),   "metAK5TypeIIPat",("muon","Pat"),("electron","Pat"),("photon","Pat") , "ak5Jet", "Calo" ]))
+
+        return { "objects": objects,
+                 "nJetsMinMax" : dict([  ("ge2",(2,None)),  ("2",(2,2)),  ("ge3",(3,None))  ] [0:1] ),
+                 "jetId" :  ["JetIDloose","JetIDtight"] [0],
+                 }
+
+    def listOfCalculables(self,params) :
+        _jet =  params["objects"]["jet"]
+        _muon = params["objects"]["muon"]
+        _photon = params["objects"]["photon"]
         
-        steps.hltFilter("HLT_Jet50U"),
-        steps.leadingUnCorrJetPtSelector([jets],100.0),
-        steps.techBitFilter([0],True),
-        steps.physicsDeclared(),
-        steps.vertexRequirementFilter(),
-        steps.monsterEventFilter(),
-        steps.hbheNoiseFilter(),
+        calcs =  calculables.zeroArgs() +\
+                calculables.fromCollections("calculablesJet",[_jet]) +\
+                calculables.fromCollections("calculablesLepton",[_muon]) +\
+                [ calculables.xcJet( _jet,  gamma = _photon, gammaDR = 0.5, 
+                                     muon = _muon, muonDR = 0.5),
+                  calculables.jetIndices( _jet, ptMin = 20.0, etaMax = 3.0, flagName = params["jetId"]),
+                 calculables.photonIndicesPat(  ptMin = 30, flagName="photonIDLoosePat"),
+                  calculables.muonIndices( _muon, ptMin = 20.0, combinedRelIsoMax = 0.15),
+                  calculables.genIndices( label = "Z",      pdgs = [23]),
+                  calculables.genIndices( label = "Photon", pdgs = [22]),
+                  ]
+        return calcs
+
+    def listOfSteps(self,params) :
+        _jet = params["objects"]["jet"]
+        _muon = params["objects"]["muon"]
+        stepList = [
+            steps.progressPrinter(),
+
+            steps.jetPtSelector(_jet,100.0,0),
+            steps.leadingUnCorrJetPtSelector( [_jet],100.0 ),
+            steps.hltFilter("HLT_Jet50U"),
+            steps.vertexRequirementFilter(5.0,24.0),
+            steps.techBitFilter([0],True),
+            steps.physicsDeclared(),
+            steps.monsterEventFilter(10,0.25),
+            steps.hbheNoiseFilter(),
         
-        steps.photonSelectionHistogrammer( nametag = "raw", matchDeltaRMax = 0.1),
-        steps.jetPtSelector(jets,100.0,0),
-        steps.jetPtSelector(jets,40.0,1),
-        steps.maxNOtherJetEventFilter(jets,0),
-        steps.minNCleanJetEventFilter(xcjets,2),
-        steps.photonSelectionHistogrammer( nametag = "jets", matchDeltaRMax = 0.1),
-        steps.alphaHistogrammer(xcjets),
-        steps.variableGreaterFilter(300.0,"%sSumPt%s"%xcjets),
-        steps.variableGreaterFilter(0.55,"%sAlphaT%s"%xcjets),
-        steps.displayer(xcjets,"met","Calo","Pat","ak5Jet","Calo",recHitPtThreshold=1.0,#GeV
-                        outputDir="/vols/cms02/%s/tmp/"%os.environ["USER"],scale=200.0),
-        ]
-    return listOfSteps
+            steps.multiplicityFilter("%sIndices%s"%_jet, nMin = params["nJetsMinMax"][0], nMax = params["nJetsMinMax"][1]),
+            steps.multiplicityFilter("%sIndicesOther%s"%_jet, nMax = 0),
+            steps.multiplicityFilter("%sIndices%s"%_muon, nMax = 0),
+            steps.variableGreaterFilter(350.0,"%sSumPt%s"%_jet),
+            steps.variableGreaterFilter(0.55,"%sAlphaT%s"%_jet),
 
-def makeCalculables() :
-    listOfCalculables = calculables.zeroArgs()
-    listOfCalculables += calculables.fromJetCollections(jetTypes)
-    listOfCalculables += calculables.fromJetCollections([("xc"+jt[0],jt[1]) for jt in jetTypes])
-    listOfCalculables += [ calculables.jetIndices( collection = jetType, ptMin = 20.0, etaMax = 3.0, flagName = "JetIDloose") for jetType in jetTypes]
-    listOfCalculables += [ calculables.jetIndicesOther( collection = jetType, ptMin = 20.0) for jetType in jetTypes]
+            # signal distributions for {g,Z}x{forward,mid,central}
 
-    listOfCalculables += [ calculables.xcJetCorrectedP4(jetType, photons = ("photon","Pat"), photonDR = 0.5) for jetType in jetTypes ]
-    listOfCalculables += [ calculables.xcJetIndices(("xc"+jt[0],jt[1])) for jt in jetTypes]
-
-    listOfCalculables += [ calculables.genIndices( label = "Z",      pdgs = [23],     ptMin = 30, etaMax = 5),
-                           calculables.genIndices( label = "Photon", pdgs = [22],     ptMin = 30, etaMax = 5),
-                           calculables.photonIndicesPat( flagName="photonIDLoosePat", ptMin = 30, etaMax = 5) ]
-    return listOfCalculables
-
-def makeSamples() :
-    from samples import specify
-    return [#specify(name = "JetMETTau.Run2010A",    nFilesMax = 1, nEventsMax = 1000, color = r.kBlack   , markerStyle = 20),
-            specify(name = "qcd_py_pt30",           nFilesMax = 1, nEventsMax = 1000, color = r.kBlue    ),
-            specify(name = "qcd_py_pt80",           nFilesMax = 1, nEventsMax = 1000, color = r.kBlue    ),
-            specify(name = "qcd_py_pt170",          nFilesMax = 1, nEventsMax = 1000, color = r.kBlue    ),
-            specify(name = "qcd_py_pt300",          nFilesMax = 1, nEventsMax = 1000, color = r.kBlue    ),
-            specify(name = "qcd_py_pt470",          nFilesMax = 1, nEventsMax = 1000, color = r.kBlue    ),
-            specify(name = "qcd_py_pt800",          nFilesMax = 1, nEventsMax = 1000, color = r.kBlue    ),
-            specify(name = "qcd_py_pt1400",         nFilesMax = 1, nEventsMax = 1000, color = r.kBlue    ),
-            specify(name = "gammajets_mg_pt40_100", nFilesMax = 1, nEventsMax = 1000, color = r.kGreen   ),
-            specify(name = "gammajets_mg_pt100_200",nFilesMax = 1, nEventsMax = 1000, color = r.kGreen   ),
-            specify(name = "gammajets_mg_pt200",    nFilesMax = 1, nEventsMax = 1000, color = r.kGreen   ),
-            specify(name = "tt_tauola_mg",          nFilesMax = 1, nEventsMax = 1000, color = r.kOrange  ),
-            specify(name = "z_inv_mg",              nFilesMax = 1, nEventsMax = 1000, color = r.kMagenta ),
-            specify(name = "z_jets_mg",             nFilesMax = 1, nEventsMax = 1000, color = r.kYellow-3),
-            specify(name = "w_jets_mg",             nFilesMax = 1, nEventsMax = 1000, color = 28         ),
-            specify(name = "lm0",                   nFilesMax = 1, nEventsMax = 1000, color = r.kRed     ),
-            specify(name = "lm1",                   nFilesMax = 1, nEventsMax = 1000, color = r.kRed+1   ),
+            #steps.photonSelectionHistogrammer( matchDeltaRMax = 0.1),
+            steps.alphaHistogrammer(_jet),
             ]
+        return stepList
+        
 
-a = analysis.analysis( name = "photonSelection",
-                       outputDir = "/vols/cms02/%s/tmp/"%os.environ["USER"],
-                       listOfSteps = makeSteps(),
-                       listOfCalculables = makeCalculables(),
-                       listOfSamples = makeSamples(),
-                       listOfSampleDictionaries = [samples.mc, samples.jetmet]
-                       )
-#a.loop( nCores = 6 )
+    def listOfSampleDictionaries(self) :
+        return [samples.mc, samples.jetmet]
 
-##plotting
-a.mergeHistograms(target="g_jets_mg", targetColor = r.kGreen, source=["gammajets_mg_pt%s"%bin for bin in ["40_100","100_200","200"] ])
-a.mergeHistograms(target="qcd_py",    targetColor = r.kBlue, source=["qcd_py_pt%d"%i         for i in [30,80,170,300,470,800,1400] ])
-a.mergeAllHistogramsExceptSome(target="standard_model",targetColor = r.kGreen, dontMergeList=["JetMETTau.Run2010A","lm0","lm1"],keepSourceHistograms=True)
+    def listOfSamples(self) :
+        from samples import specify
+        return [  specify(name = "JetMET_skim",           nFilesMax = 1, color = r.kBlack   , markerStyle = 20),
+                ##specify(name = "qcd_mg_ht_250_500_old", nFilesMax = 1, color = r.kBlue    ),
+                ##specify(name = "qcd_py_pt30",           nFilesMax = 1, color = r.kBlue    ),
+                  specify(name = "qcd_py_pt80",           nFilesMax = 1, color = r.kBlue    ),
+                  specify(name = "qcd_py_pt170",          nFilesMax = 1, color = r.kBlue    ),
+                  specify(name = "qcd_py_pt300",          nFilesMax = 1, color = r.kBlue    ),
+                ##specify(name = "qcd_py_pt470",          nFilesMax = 1, color = r.kBlue    ),
+                ##specify(name = "qcd_py_pt800",          nFilesMax = 1, color = r.kBlue    ),
+                ##specify(name = "qcd_py_pt1400",         nFilesMax = 1, color = r.kBlue    ),
+                  specify(name = "tt_tauola_mg",          nFilesMax = 1, color = r.kOrange  ),
+                  specify(name = "g_jets_mg_pt40_100",    nFilesMax = 1, color = r.kGreen   ),
+                  specify(name = "g_jets_mg_pt100_200",   nFilesMax = 1, color = r.kGreen   ),
+                  specify(name = "g_jets_mg_pt200",       nFilesMax = 1, color = r.kGreen   ),
+                  specify(name = "z_inv_mg_skim",         nFilesMax = 1, color = r.kMagenta ),
+                  specify(name = "z_jets_mg_skim",        nFilesMax = 1, color = r.kYellow-3),
+                  specify(name = "w_jets_mg_skim",        nFilesMax = 1, color = 28         ),
+                  specify(name = "lm0",                   nFilesMax = 1, color = r.kRed     ),
+                  specify(name = "lm1",                   nFilesMax = 1, color = r.kRed+1   ),
+                  ]
 
-plotter.plotAll(hyphens=a.hyphens,
-                listOfPlotContainers=a.organizeHistograms(),
-                psFileName=a.outputDir+"/"+a.name+".ps",
-                samplesForRatios=("JetMETTau.Run2010A","qcd_py"),
-                sampleLabelsForRatios=("data","sim"),
-                )
+    def conclude(self) :
+        for tag in self.sideBySideAnalysisTags() :
+            #organize
+            org=organizer.organizer( self.sampleSpecs(tag) )
+            org.mergeSamples(targetSpec = {"name":"g_jets_mg",     "color":r.kGreen},   sources = ["g_jets_mg_pt%s"%bin for bin in ["40_100","100_200","200"] ])
+            org.mergeSamples(targetSpec = {"name":"qcd_py"   ,     "color":r.kBlue},    sources = ["qcd_py_pt%d"%i         for i in [30,80,170,300,470,800,1400] ])
+            org.mergeSamples(targetSpec = {"name":"standard_model","color":r.kGreen+3},
+                             sources = ["g_jets_mg","qcd_py","tt_tauola_mg","z_inv_mg_skim","z_jets_mg_skim","w_jets_mg_skim"], keepSources = True
+                             )
+            org.scale()
+
+            #plot
+            pl = plotter.plotter(org,
+                                 psFileName = self.psFileName(tag),
+                                 samplesForRatios=("JetMET_skim","standard_model"),
+                                 sampleLabelsForRatios=("data","s.m."),
+                                 )
+            pl.plotAll()
+
