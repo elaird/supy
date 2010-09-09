36a70c8e225a05333a05ece5cd4beedc3ece0582
elaird <elaird>
2010-09-09 13:46:43 +0000

    minor analyses

---
+++ analyses/genMLook.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os,analysis,steps,calculables,samples,organizer,plotter
+import ROOT as r
+
+class genMLook(analysis.analysis) :
+    def baseOutputDirectory(self) :
+        return "/vols/cms02/%s/tmp/"%os.environ["USER"]
+
+    def listOfCalculables(self,params) :
+        return calculables.zeroArgs()
+
+    def listOfSteps(self,params) :
+        outList=[
+            steps.progressPrinter(),
+            #steps.genParticlePrinter(minPt = -1.0, minStatus = 3),
+            #steps.genMassHistogrammer(),
+            steps.genSHatHistogrammer(),
+            ]
+        return outList
+
+    def listOfSampleDictionaries(self) :
+        return [samples.mc, samples.jetmet]
+
+    def listOfSamples(self,params) :
+        from samples import specify
+        outList =[
+            specify(name = "z_jets_mg", nFilesMax = 1, nEventsMax = 10, color = r.kYellow-3),
+            ]
+        return outList
+
+    def conclude(self) :
+        org=organizer.organizer( self.sampleSpecs() )
+        org.scale(100.0)
+
+        pl = plotter.plotter(org,
+                             psFileName = self.psFileName(""),
+                             )
+        pl.plotAll()
