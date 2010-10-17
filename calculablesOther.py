c7275b81fdd592c19640170100f7361711a02f36
bbetchar <bbetchar>
2010-10-17 08:20:51 +0000

    vertex calculables

--- calculablesOther.py
+++ calculablesOther.py
@@ -1,4 +1,5 @@
 from wrappedChain import *
+import calculables
 
 class localEntry(wrappedChain.calculable) :
     def update(self,localEntry) :
@@ -34,9 +35,49 @@ class deadEcalRegionsFromFile(wrappedChain.calculable) :
         self.value={}
         self.value["trigPrims"] = self.trigPrims
         self.value["nBadXtals"] = self.nBadXtals
-
+##############################                        
 class ecalDeadTowerTrigPrimP4(wrappedChain.calculable) :
     def update(self,ignored) : self.value = self.source["deadEcalRegionsFromFile"]["trigPrims"]
         
 class ecalDeadTowerNBadXtals(wrappedChain.calculable) :
     def update(self,ignored) : self.value = self.source["deadEcalRegionsFromFile"]["nBadXtals"]
+
+class ecalDeadTowerIsBarrel(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = map( self.isBarrel, self.source["ecalDeadTowerTrigPrimP4"] )
+    def isBarrel(self, p4) : return abs(p4.eta()) < 1.48
+##############################                        
+class vertexID(wrappedChain.calculable) :
+    def __init__(self, minNdof = 5.0, maxAbsZ = 24.0, maxD0 = 2.0 ) :
+        for item in ["minNdof","maxAbsZ","maxD0"] : setattr(self,item,eval(item))
+        self.moreName = "!fake; nd>=%.1f; |z|<=%.1f cm; d0<=%.1f cm" % (minNdof,maxAbsZ,maxD0)
+
+    def id(self, isFake, ndof, pos) :
+        return (not isFake) and \
+               ndof >= self.minNdof and \
+               abs(pos.Z()) <= self.maxAbsZ and \
+               abs(pos.Rho()) <= self.maxD0
+
+    def update(self,ignored) :
+        self.value = map(self.id, self.source["vertexIsFake"],self.source["vertexNdof"],self.source["vertexPosition"])
+
+class vertexIndices(wrappedChain.calculable) :
+    def __init__(self, sumPtMin = None) :
+        self.sumPtMin = sumPtMin
+        self.moreName = "sumPt >=%.1f; pass ID"%sumPtMin
+        
+    def update(self,ignored) :
+        sumPt = self.source["vertexSumPt"]
+        id = self.source["vertexID"]
+        self.value = []
+        other = self.source["vertexIndicesOther"]
+        for i in range(sumPt.size()) :
+            if sumPt.at(i) < self.sumPtMin : continue
+            elif id[i] : self.value.append(i)
+            else : other.append(i)
+        self.value.sort( key = sumPt.__getitem__, reverse = True )
+        
+class vertexIndicesOther(calculables.indicesOther) :
+    def __init__(self) :
+        super(vertexIndicesOther, self).__init__(("vertex",""))
+        self.moreName = "pass sumPtMin; fail ID"
+
