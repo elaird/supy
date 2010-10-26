87722fdb38d9c385de6f4d0bbc18f7d938132a73
elaird <elaird>
2010-10-26 16:04:44 +0000

    lumi

--- samplesMuon.py
+++ samplesMuon.py
@@ -4,7 +4,7 @@ muon = samples.SampleHolder()
 srm = 'utils.fileListFromSrmLs(location="/pnfs/hep.ph.ic.ac.uk/data/cms/store/user'
 
 #38X SKIMS
-muon.add("Mu.Run2010_skim", 'utils.fileListFromDisk(location = "/vols/cms02/elaird1/11_skims/17_markus_mu_dataset/")', lumi = 999999.9) #/pb
+muon.add("Mu.Run2010_skim", 'utils.fileListFromDisk(location = "/vols/cms02/elaird1/11_skims/17_markus_mu_dataset/")', lumi = 14.511) #/pb
 
 #38X ORIGINALS
 muon.add("Mu.Run2010A-Sep17ReReco_v2.RECO.Robin",             '%s/rnandi//ICF/automated/2010_10_13_14_30_58/")'%srm, lumi = 99999.9 )
