35933cedfc3ddf545fa431d016600be208395e7a
elaird <elaird>
2010-11-18 07:30:50 +0000

    new binning for comb iso

--- stepsPhoton.py
+++ stepsPhoton.py
@@ -190,7 +190,7 @@ class singlePhotonHistogrammer(analysisStep) :
 
             #combIso
             book.fill(cmbI, "%s%s%scombinedIsolation"%(self.cs+(photonLabel,)),
-                      50, 0.0, 10.0, title=";combined isolation (GeV);events / bin")
+                      50, 0.0, 20.0, title=";combined isolation (GeV);events / bin")
 
             #sHH
             if abs(photon.eta())<self.etaBE :
