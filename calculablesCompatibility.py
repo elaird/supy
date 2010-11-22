2d1dd3acd7b485e85ea78ce0d83ac26aef5c0309
elaird <elaird>
2010-11-22 12:52:39 +0000

    move compat. calcs to new file

---
+++ calculablesCompatibility.py
@@ -0,0 +1,78 @@
+from wrappedChain import *
+import calculables
+##############################
+class deadEcalRegionsFromFile(wrappedChain.calculable) :
+    def __init__(self) :
+        self.trigPrims = r.std.vector(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double')))()
+        self.nBadXtals = r.std.vector("int")()
+        inFile=open("deadRegionList.txt")
+        for line in inFile :
+            if line[0]=="#" : continue
+            fieldList = line.split()
+            eta  = float(fieldList[0])
+            phi  = float(fieldList[1])
+            nBad = int(fieldList[4])
+            self.trigPrims.push_back(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double'))(0.0,eta,phi,0.0))
+            self.nBadXtals.push_back(nBad)
+        inFile.close()
+
+    def update(self,ignored) :
+        self.value={}
+        self.value["trigPrims"] = self.trigPrims
+        self.value["nBadXtals"] = self.nBadXtals
+##############################
+class deadHcalChannelsFromFile(wrappedChain.calculable) :
+    def __init__(self) :
+        self.p4s = r.std.vector(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double')))()
+        self.status = r.std.vector("int")()
+        inFile = open("hcalDeadChannels.txt")
+        for line in inFile :
+            if line[0]=="#" : continue
+            fieldList = line.split()
+            if not len(fieldList) : continue
+            eta    = float(fieldList[0])
+            phi    = float(fieldList[1])
+            status = int(fieldList[2])
+            self.p4s.push_back(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double'))(0.0, eta, phi, 0.0))
+            self.status.push_back(status)
+        inFile.close()
+
+    def update(self,ignored) :
+        self.value={}
+        self.value["p4"] = self.p4s
+        self.value["status"] = self.status
+##############################
+class ecalDeadTowerTrigPrimP4(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = self.source["deadEcalRegionsFromFile"]["trigPrims"]
+##############################
+class ecalDeadTowerNBadXtals(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = self.source["deadEcalRegionsFromFile"]["nBadXtals"]
+##############################
+class hcalDeadChannelP4(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = self.source["deadHcalChannelsFromFile"]["p4"]
+##############################
+class hcalDeadChannelStatus(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = self.source["deadHcalChannelsFromFile"]["status"]
+##############################
+class logErrorTooManyClusters(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class logErrorTooManySeeds(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+##############################
+class beamHaloCSCLooseHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class beamHaloCSCTightHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class beamHaloEcalLooseHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class beamHaloEcalTightHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class beamHaloGlobalLooseHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class beamHaloGlobalTightHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class beamHaloHcalLooseHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+class beamHaloHcalTightHaloId(wrappedChain.calculable) :
+    def update(self,ignored) : self.value = False
+##############################
--- calculablesOther.py
+++ calculablesOther.py
@@ -1,78 +1,25 @@
 from wrappedChain import *
 import calculables
-
+#####################################
 class localEntry(wrappedChain.calculable) :
     def update(self,localEntry) :
         self.value = localEntry
-
+#####################################
 class entry(wrappedChain.calculable) :
     def update(self,localEntry) :
         self.value = self.source.entry
-
+#####################################
 class chain_access(wrappedChain.calculable) :
     def name(self) : return "chain"
     def update(self,ignored) : self.value = self.source._wrappedChain__chain
-
+#####################################
 class crock(wrappedChain.calculable) :
     def update(self,localEntry) : self.value = {}
-##############################
-class deadEcalRegionsFromFile(wrappedChain.calculable) :
-    def __init__(self) :
-        self.trigPrims = r.std.vector(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double')))()
-        self.nBadXtals = r.std.vector("int")()
-        inFile=open("deadRegionList.txt")
-        for line in inFile :
-            if line[0]=="#" : continue
-            fieldList = line.split()
-            eta  = float(fieldList[0])
-            phi  = float(fieldList[1])
-            nBad = int(fieldList[4])
-            self.trigPrims.push_back(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double'))(0.0,eta,phi,0.0))
-            self.nBadXtals.push_back(nBad)
-        inFile.close()
-
-    def update(self,ignored) :
-        self.value={}
-        self.value["trigPrims"] = self.trigPrims
-        self.value["nBadXtals"] = self.nBadXtals
-##############################
-class deadHcalChannelsFromFile(wrappedChain.calculable) :
-    def __init__(self) :
-        self.p4s = r.std.vector(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double')))()
-        self.status = r.std.vector("int")()
-        inFile = open("hcalDeadChannels.txt")
-        for line in inFile :
-            if line[0]=="#" : continue
-            fieldList = line.split()
-            if not len(fieldList) : continue
-            eta    = float(fieldList[0])
-            phi    = float(fieldList[1])
-            status = int(fieldList[2])
-            self.p4s.push_back(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double'))(0.0, eta, phi, 0.0))
-            self.status.push_back(status)
-        inFile.close()
-
-    def update(self,ignored) :
-        self.value={}
-        self.value["p4"] = self.p4s
-        self.value["status"] = self.status
-##############################
-class ecalDeadTowerTrigPrimP4(wrappedChain.calculable) :
-    def update(self,ignored) : self.value = self.source["deadEcalRegionsFromFile"]["trigPrims"]
-        
-class ecalDeadTowerNBadXtals(wrappedChain.calculable) :
-    def update(self,ignored) : self.value = self.source["deadEcalRegionsFromFile"]["nBadXtals"]
-
+#####################################
 class ecalDeadTowerIsBarrel(wrappedChain.calculable) :
     def update(self,ignored) : self.value = map( self.isBarrel, self.source["ecalDeadTowerTrigPrimP4"] )
     def isBarrel(self, p4) : return abs(p4.eta()) < 1.48
-##############################                        
-class hcalDeadChannelP4(wrappedChain.calculable) :
-    def update(self,ignored) : self.value = self.source["deadHcalChannelsFromFile"]["p4"]
-
-class hcalDeadChannelStatus(wrappedChain.calculable) :
-    def update(self,ignored) : self.value = self.source["deadHcalChannelsFromFile"]["status"]
-##############################                        
+#####################################
 class vertexID(wrappedChain.calculable) :
     def __init__(self, minNdof = 5.0, maxAbsZ = 24.0, maxD0 = 2.0 ) :
         for item in ["minNdof","maxAbsZ","maxD0"] : setattr(self,item,eval(item))
@@ -86,7 +33,7 @@ class vertexID(wrappedChain.calculable) :
 
     def update(self,ignored) :
         self.value = map(self.id, self.source["vertexIsFake"],self.source["vertexNdof"],self.source["vertexPosition"])
-
+#####################################
 class vertexIndices(wrappedChain.calculable) :
     def __init__(self, sumPtMin = None) :
         self.sumPtMin = sumPtMin
@@ -105,12 +52,12 @@ class vertexIndices(wrappedChain.calculable) :
             elif id[i] : self.value.append(i)
             else : other.append(i)
         self.value.sort( key = sumPt.__getitem__, reverse = True )
-        
+#####################################
 class vertexIndicesOther(calculables.indicesOther) :
     def __init__(self) :
         super(vertexIndicesOther, self).__init__(("vertex",""))
         self.moreName = "pass sumPtMin; fail ID"
-
+#####################################
 class vertexSumPt(wrappedChain.calculable) :
     def __init__(self) :
         self.sumPts = r.std.vector('double')()
@@ -119,10 +66,23 @@ class vertexSumPt(wrappedChain.calculable) :
             
     def update(self, ignored) :
         self.value = self.sumPts
-
+#####################################
 class vertexSumP3(wrappedChain.calculable) :
     def __init__(self) :
         self.sumP3s = r.std.vector(r.Math.LorentzVector(r.Math.PtEtaPhiE4D('double')))()
 
     def update(self, ignored) :
         self.value = self.sumP3s
+#####################################
+class lowestUnPrescaledTrigger(wrappedChain.calculable) :
+    def __init__(self, sortedListOfPaths = []) :
+        self.sortedListOfPaths = sortedListOfPaths
+        self.moreName = "lowest unprescaled of "+','.join(self.sortedListOfPaths).replace("HLT_","")
+        
+    def update(self, ignored) :
+        self.value = None
+        for path in self.sortedListOfPaths :
+            if self.source["prescaled"][path]==1 :
+                self.value = path
+                break
+#####################################
