ab77e85284aa165f3128a5dd0ad4d09280b0b9e4
elaird <elaird>
2010-11-30 15:00:55 +0000

    move to configuration

--- batchConf.py
+++
@@ -1,7 +0,0 @@
-d = {"lx05.hep.ph.ic.ac.uk":("icSub.sh","icJob.sh"),
-     "lx06.hep.ph.ic.ac.uk":("icSub.sh","icJob.sh"),
-     }
-
-def scripts(hostName) :
-    assert hostName in d,"hostname %s not recognized"%hostName
-    return d[hostName]
