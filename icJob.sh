edcb9a05d13fda1e1a8fde50c24a3fb42639bf78
elaird <elaird>
2010-12-10 20:36:57 +0000

    update CMSSW

--- icJob.sh
+++ icJob.sh
@@ -2,7 +2,7 @@
 
 export X509_USER_PROXY=/home/hep/$USER/.MyProxy
 #USERTMP="/vols/cms02/$USER/tmp"
-CMSSW=/vols/sl5_exp_software/cms/slc5_ia32_gcc434/cms/cmssw/CMSSW_3_7_0/src
+CMSSW=/vols/sl5_exp_software/cms/slc5_ia32_gcc434/cms/cmssw/CMSSW_3_8_5/src
 #mkdir -p $USERTMP >& /dev/null
 . /vols/cms/grid/setup.sh
 cd $CMSSW && eval `scram runtime -sh` && cd - >& /dev/null
