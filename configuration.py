f2e17889c479dcd348bf0187ad5f4dd5c45cacca
elaird <elaird>
2010-12-30 04:21:56 +0000

    lists of steps to disable

--- configuration.py
+++ configuration.py
@@ -7,6 +7,18 @@ def samplesFiles() :
 def stepsFiles() :
     return ["Other", "Jet", "Trigger", "Photon", "Print", "Gen", "Xclean", "Displayer"]
 
+def stepsToDisableForData() :
+    return ["genMotherHistogrammer", "photonPurityPlots", "photonEfficiencyPlots"]
+
+def histogramsToDisableForData() :
+    return ["genpthat"]
+
+def stepsToDisableForMc() :
+    return ["hltFilter", "hltFilterList", "lowestUnPrescaledTrigger", "lowestUnPrescaledTriggerHistogrammer", "hbheNoiseFilter", "bxFilter", "physicsDeclared", "techBitFilter"]
+
+def histogramsToDisableForMc() :
+    return []
+
 def sourceFiles() :
     return ["pragmas.h","helpers.C"]
 
