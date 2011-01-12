3b24b70530d70ba39f4acd5d8a694709fa8e58d3
elaird <elaird>
2011-01-12 23:17:46 +0000

    nPass and nFail are member functions, books ignore eventVars

--- analysisStep.py
+++ analysisStep.py
@@ -23,55 +23,32 @@ class analysisStep(object) :
             return True
 
         passed = bool(self.select(eventVars))
-        self.book(eventVars).fill(passed,"counts",2,0,2)
+        self.increment(passed)
         return passed
-
-    def setup(self, inputChain, fileDirectory, name, outputDir) :
-        return
-
-    def endFunc(self, otherChainDict) :
-        return
-
-    def mergeFunc(self, productList, someLooper) :
-        return
-
-    def name(self) :
-        return self.__class__.__name__
-
-    def name1(self) :
-        return self.name().ljust(self.docWidth)+self.moreName.ljust(self.moreWidth)
-
-    def name2(self) :
-        return "" if self.moreName2=="" else "\n"+"".ljust(self.docWidth)+self.moreName2.ljust(self.moreWidth)
-    
-    def varsToPickle(self) :
-        return []
-    
-    def disable(self) :
-        self.disabled=True
-        
-    def ignore(self) :
-        self.nPass = 0
-        self.nFail = 0
-        self.ignoreInAccounting = True
-        
-    def makeQuiet(self) :
-        self.quietMode=True
+    def increment(self, passed, w = 1) : self.book().fill(passed, "counts", 2, 0, 2, w = w)
+    def setup(self, inputChain, fileDirectory, name, outputDir) : return
+    def endFunc(self, otherChainDict) : return
+    def mergeFunc(self, productList, someLooper) : return
+    def name(self) : return self.__class__.__name__
+    def name1(self) : return self.name().ljust(self.docWidth)+self.moreName.ljust(self.moreWidth)
+    def name2(self) : return "" if self.moreName2=="" else "\n"+"".ljust(self.docWidth)+self.moreName2.ljust(self.moreWidth)
+    def varsToPickle(self) : return []
+    def requiresNoSetBranchAddress(self) : return False
+    def disable(self) : self.disabled = True
+    def makeQuiet(self) : self.quietMode = True
+    def nFromHisto(self, bin) : return self.book()["counts"].GetBinContent(bin) if "counts" in self.book() and self.book()["counts"] else 0.0
+    def nFail(self) :  return int(self.nFromHisto(1))
+    def nPass(self) :  return int(self.nFromHisto(2))
+    def ignore(self) : self.ignoreInAccounting = True
         
     def printStatistics(self) :
-        passString="-" if self.disabled else str(self.nPass)
-        failString="-" if self.disabled else str(self.nFail)
+        passString="-" if self.disabled else str(self.nPass())
+        failString="-" if self.disabled else str(self.nFail())
 
         statString = "" if not hasattr(self,"select") else \
                      "  %s %s" % ( passString.rjust(self.integerWidth), ("("+failString+")").rjust(self.integerWidth+2) )
         print "%s%s%s" % (self.name1(),self.name2(),statString)
 
-    def book(self,eventVars) :
-        if not self.needToConsiderPtHatThresholds :
-            return self.books[None]
-        else :
-            if not eventVars["genGenInfoHandleValid"] :
-                raise Exception("pt hat is needed but its handle is not valid")
-            index=bisect(self.ptHatThresholds,eventVars["genpthat"])
-            return self.books[index]
+    def book(self, eventVars = None) :
+        return self.books[None]
 #####################################
