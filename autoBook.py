dbf5f49d3c1c47033cf9158fb8e5068326da6fa4
elaird <elaird>
2011-01-12 17:01:05 +0000

    support for axis labels

--- autoBook.py
+++ autoBook.py
@@ -6,7 +6,7 @@ class autoBook(dict) :
         self.title = self.__directory.GetName() if self.__directory else ""
         self.fillOrder = []
         
-    def fill(self,x, name, N, low, up, w=1, title="") :
+    def fill(self, x, name, N, low, up, w = 1, title = "", xAxisLabels = [], yAxisLabels = []) :
         if not name in self :
             self.__directory.cd()
             self.fillOrder.append(name)
@@ -16,7 +16,11 @@ class autoBook(dict) :
                        r.TH2D( name,title, N[0],low[0],up[0],  N[1],low[1],up[1])  if len(N)==2      else \
                        r.TH3D( name,title, N[0],low[0],up[0],  N[1],low[1],up[1],  N[2],low[2],up[2])
             if not self[name].GetSumw2N() : self[name].Sumw2()
-            
+            for i,label in enumerate(xAxisLabels) :
+                self[name].GetXaxis().SetBinLabel(i+1, label)
+            for i,label in enumerate(yAxisLabels) :
+                self[name].GetYaxis().SetBinLabel(i+1, label)
+                             
         if    type(x)!=tuple : self[name].Fill(x,w)
         elif  type(N)!=tuple : self[name].Fill(x[0],x[1],w)
         elif  len(N)==2      : self[name].Fill(x[0],x[1],w)
