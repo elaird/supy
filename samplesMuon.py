32e7567add0a4c59b876d9f525874c35b2ff6b51
elaird <elaird>
2011-01-12 06:24:13 +0000

    add old skim

--- samplesMuon.py
+++ samplesMuon.py
@@ -5,6 +5,7 @@ srm = 'utils.fileListFromSrmLs(location="/pnfs/hep.ph.ic.ac.uk/data/cms/store/us
 
 #38X SKIMS
 muon.add("Mu.Run2010_skim", 'utils.fileListFromDisk(location = "/vols/cms02/elaird1/11_skims/17_markus_mu_dataset/")', lumi = 14.511) #/pb
+muon.add("Run2010_muonSkim", 'utils.fileListFromDisk(location = "/vols/cms02/elaird1/11_skims/10_markus_muons/*_skim.root", isDirectory = False)', lumi = 32) #/pb #fake number
 
 #38X ORIGINALS
 muon.add("Mu.Run2010A-Sep17ReReco_v2.RECO.Robin",             '%s/rnandi//ICF/automated/2010_10_13_14_30_58/")'%srm, lumi = 99999.9 )
