a8edd2c086fdb3221925cefd4d1bd10a384dc920
bbetchar <bbetchar>
2011-01-24 19:43:31 +0000

    baseOutputDirectory

--- analyses/triggers.py
+++ analyses/triggers.py
@@ -4,9 +4,6 @@ import os,analysis,steps,calculables,samples,organizer,plotter,utils
 import ROOT as r
 
 class triggers(analysis.analysis) :
-    def baseOutputDirectory(self) :
-        return "/vols/cms02/%s/tmp/"%os.environ["USER"]
-
     def parameters(self) :
         return { }
     
