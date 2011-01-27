df63ca8fb4813dbbf8df2d4f40079f3207bbc291
bbetchar <bbetchar>
2011-01-27 04:53:04 +0000

    Add w_munu, with unknown xs

--- samplesMC.py
+++ samplesMC.py
@@ -170,6 +170,8 @@ mc.add("w_jets_mg_v12",'%s/jad/ICF/automated//2010_06_18_22_33_23/")'%srm,
        xs = {"LO":24170.0,"NNLO":31314.0}["NNLO"] )
 mc.add("zg_nunu", '%s/bbetchar/ICF/automated/2010_10_09_21_40_58/")'%srm, xs = 2.68 )
 
+mc.add("w_munu",'%s/trommers/ICF/automated/2010_10_06_18_40_51/")'%srm, xs = 10000) # DUMMY XS !!!!
+
 #SUSY
 mc.add("lm0_v12" ,'%s/bainbrid/ICF/automated/2010_07_16_12_54_00/LM0.Spring10-START3X_V26_S09-v1.GEN-SIM-RECO/")'%srm,  xs = 38.93   )
 mc.add("lm1_v12" ,'%s/bainbrid/ICF/automated/2010_07_12_17_52_54/LM1.Spring10-START3X_V26_S09-v1.GEN-SIM-RECO/")'%srm,  xs = 4.888   )
