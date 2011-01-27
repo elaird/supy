fd58809cef35fc8f62b2b49765076445b54febb6
bbetchar <bbetchar>
2011-01-27 04:54:30 +0000

    Use SumP4 instead of MissingP4, and validate

--- calculablesOther.py
+++ calculablesOther.py
@@ -62,57 +62,72 @@ class Mt(wrappedChain.calculable) :
 class SemileptonicTopIndex(wrappedChain.calculable) :
     def __init__(self, collection = None) :
         self.fixes = collection
-        self.stash(["Indices"])
-        self.moreName = "Just indices[0] for now."
+        self.stash(["Indices","IndicesNonIso"])
+        self.moreName = "Just indices[0] or indicesNonIso[0] for now."
 
     def update(self,ignored) :
         indices = self.source[self.Indices]
-        self.value = indices[0] if indices else None
+        nonIso = self.source[self.IndicesNonIso]
+        self.value = indices[0] if indices else nonIso[0] if nonIso else None
 #####################################
 class RelativeRapidity(wrappedChain.calculable) :
-    def __init__(self, collection = None, MissingP4 = None, NuP4 = None) :
+    def name(self) : return "%s%s"%self.fixes + self.__class__.__name__ + self.SumP4
+    def __init__(self, collection = None, SumP4 = None) :
         self.fixes = collection
+        self.SumP4 = SumP4
         self.stash(["SemileptonicTopIndex","P4","Charge"])
-        self.MissingP4 = MissingP4
-        self.NuP4 = NuP4
-        self.moreName = "- sign(y_miss) * q_lep * (y_miss+y_lep); %s%s; %s"%(collection+("%s-%s"(MissingP4,NuP4),))
+        self.moreName = "sign(y_sum) * q_lep * (y_lep-y_sum); %s%s; %s"%(collection+(SumP4,))
 
     def update(self,ignored) :
         index = self.source[self.SemileptonicTopIndex]
         if index is None: self.value=None; return
 
-        miss = self.source[self.MissingP4]
-        y_miss = miss.rapidity() if not self.NuP4 else (miss-self.source[self.NuP4]).rapidity()
-        y_lep = self.source[self.P4][index].rapidity()
+        y_sum = self.source[self.SumP4].Rapidity()
+        y_lep = self.source[self.P4][index].Rapidity()
         q_lep = self.source[self.Charge][index]
         
-        self.value = (-1 if y_miss>0 else 1) * q_lep * (y_miss + y_lep)
+        self.value = (1 if y_sum>0 else -1) * q_lep * (y_lep-y_sum)
 #####################################
 class NeutrinoP4(wrappedChain.calculable) :
-    def __init__(self, collection = None, MissingP4 = None, solutionPz = None) :
+    def __init__(self, collection, SumP4, solutionPz) :
         self.fixes = collection
         self.stash(["NeutrinoPz"])
-        self.MissingP4 = MissingP4
+        self.SumP4 = SumP4
         self.solutionPz = 1 if solutionPz>0 else 0
 
     def update(self,ignored) :
         self.value = None
         pzMinusPlus = self.source[self.NeutrinoPz]
         if not pzMinusPlus : return
-        missing = self.source[self.MissingP4]
-        px,py,pz = missing.px(),missing.py(),pzMinusPlus[self.solutionPz]
-        self.value = type(self.source[self.MissingP4])().SetPxPyPzE(px,py,pz,math.sqrt(px**2+py**2+pz**2))
-class NeutrinoPlusP4(NeutrinoP4) :
-    def __init__(self, collection = None, MissingP4 = None) : super(NeutrinoPlusP4,self).__init__(collection,MissingP4,+1)
-class NeutrinoMinusP4(NeutrinoP4) :
-    def __init__(self, collection = None, MissingP4 = None) : super(NeutrinoPlusP4,self).__init__(collection,MissingP4,-1)
+        sum = self.source[self.SumP4]
+        px,py,pz = -sum.px(),-sum.py(),pzMinusPlus[self.solutionPz]
+        self.value = type(self.source[self.SumP4])().SetPxPyPzE(px,py,pz,math.sqrt(px**2+py**2+pz**2))
+class NeutrinoP4P(NeutrinoP4) :
+    def __init__(self, collection = None, SumP4 = None) : super(NeutrinoP4P,self).__init__(collection,SumP4,+1)
+class NeutrinoP4M(NeutrinoP4) :
+    def __init__(self, collection = None, SumP4 = None) : super(NeutrinoP4M,self).__init__(collection,SumP4,-1)
+######################################
+class SumP4NuX(wrappedChain.calculable) :
+    def name(self) : return self.SumP4 + "Nu" + self.solutionPz
+    def __init__(self, collection, SumP4, solutionPz) :
+        self.NuX = ("%sNeutrinoP4"+solutionPz+"%s")%collection
+        self.SumP4 = SumP4
+        self.solutionPz = solutionPz
+    def update(self,ignored) :
+        nu = self.source[self.NuX]
+        if nu : self.value = nu + self.source[self.SumP4]
+        else : self.value = self.source[self.SumP4]
+class SumP4NuP(SumP4NuX) :
+    def __init__(self, collection = None, SumP4 = None) : super(SumP4NuP,self).__init__(collection,SumP4,"P")
+class SumP4NuM(SumP4NuX) :
+    def __init__(self, collection = None, SumP4 = None) : super(SumP4NuM,self).__init__(collection,SumP4,"M")
 ######################################
 class NeutrinoPz(wrappedChain.calculable) :
-    def __init__(self, collection = None, MissingP4 = None) :
+    def __init__(self, collection = None, SumP4 = None) :
         self.fixes = collection
         self.stash(["SemileptonicTopIndex","P4"])
-        self.MissingP4 = MissingP4
-        self.moreName = "neutrinoPz; given SemileptonicTopIndex in %s%s; %s"%(collection+(MissingP4,))
+        self.SumP4 = SumP4
+        self.moreName = "neutrinoPz; given SemileptonicTopIndex in %s%s; %s"%(collection+(SumP4,))
         self.Wmass2 = 80.4**2 # GeV
         
     def update(self,ignored) :
@@ -122,19 +137,19 @@ class NeutrinoPz(wrappedChain.calculable) :
         if index is None: return
 
         lep4 = self.source[self.P4][index]
-        nu4 = self.source[self.MissingP4]
-        W4 = lep4 + nu4
+        negNu4 = self.source[self.SumP4]
+        W4 = lep4 - negNu4
 
         lepZ = lep4.z()
         lepE = lep4.E()
         lepT2 = lep4.Perp2()
-        nuT2 = nu4.Perp2()
+        nuT2 = negNu4.Perp2()
         WT2 = W4.Perp2()
 
         P = self.Wmass2 + WT2 - lepT2 - nuT2
 
         discriminant = lepZ**2 + 4*lepT2*(1-(4*lepE*lepE*nuT2)/(P**2))
-        if discriminant < 0: return
+        if discriminant < 0: discriminant = 0
         
         sqrtD = math.sqrt(discriminant)
         factor = 0.5*P/lepT2
