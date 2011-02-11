230fb698bc1023e242e4895803ff9dd598bfec2a
bbetchar <bbetchar>
2011-02-11 08:15:15 +0000

    skims

--- samplesElectron.py
+++ samplesElectron.py
@@ -2,6 +2,13 @@ import samples
 from configuration import srm
 electron = samples.SampleHolder()
 
+#Skims
+electron.add("EG.2010A_skim",
+             'utils.fileListFromDisk(localtion = "/vols/cms02/bbetchar/01_skims/EG.Run2010A-Nov4ReReco_v1.RECO.Sparrow/")', lumi = 9999)
+electron.add("Electron.Run2010B_skim",
+             'utils.fileListFromDisk(location = "/vols/cms02/bbetchar/01_skims/Electron.Run2010B-Nov4ReReco_v1.RECO.Sparrow")', lumi = 9999)
+
+
 #38X ORIGINALS
 electron.add("Electron.Run2010B-Nov4ReReco_v1.RECO.Sparrow",'%s/as1604//ICF/automated/2011_01_07_14_15_11/")'%srm, lumi = 9999)
 electron.add("EG.Run2010A-Nov4ReReco_v1.RECO.Sparrow",'%s/as1604//ICF/automated/2011_01_07_14_21_29/")'%srm, lumi = 9999)
