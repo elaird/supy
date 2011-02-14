7b45010c83849de8a7fce8123af695e096d184c2
bbetchar <bbetchar>
2011-02-14 21:27:56 +0000

    electron

--- analyses/triggers.py
+++ analyses/triggers.py
@@ -5,45 +5,52 @@ import ROOT as r
 
 class triggers(analysis.analysis) :
     def parameters(self) :
-        return { "muon" : ("muon","Pat") }
+        return { "muon" : ("muon","Pat"),
+                 "electron":("electron","Pat") }
     
-    def listOfCalculables(self, params) :
+    def listOfCalculables(self, pars) :
         outList  = calculables.zeroArgs()
-        outList += calculables.fromCollections(calculables.Muon, [params["muon"]])
-        outList +=[calculables.Muon.Indices( params["muon"], ptMin = 5, combinedRelIsoMax = 0.15),
+        outList += calculables.fromCollections(calculables.Muon, [pars["muon"]])
+        outList += calculables.fromCollections(calculables.Electron, [pars["electron"]])
+        outList +=[calculables.Muon.Indices( pars["muon"], ptMin = 5, combinedRelIsoMax = 0.15),
+                   calculables.Electron.Indices( pars["electron"], ptMin = 5, simpleEleID = "95", useCombinedIso = True)
                    ]
         return outList
     
-    def listOfSteps(self, params) :
+    def listOfSteps(self, pars) :
         return (
-            [steps.Print.progressPrinter()] +
-            steps.Other.multiplicityPlotFilter("%sIndices%s"%params["muon"], nMin = 1, xlabel = "N muons") +
-            [steps.Trigger.triggerScan( pattern = r"HLT_Mu\d*($|_v\d*|_HT\d*U|_Jet\d*U)", prescaleRequirement = "prescale==1", tag = "Mu"),
-             steps.Trigger.triggerScan( pattern = r"HLT_Mu\d($|\d($|_v\d*))", prescaleRequirement = "True", tag = "MuAll"),
+            [steps.Print.progressPrinter(),
+             steps.Filter.multiplicity("%sIndices%s"%pars["electron"], min = 1),
+             #steps.Trigger.triggerScan( pattern = r"HLT_Mu\d*($|_v\d*|_HT\d*U|_Jet\d*U)", prescaleRequirement = "prescale==1", tag = "Mu"),
+             #steps.Trigger.triggerScan( pattern = r"HLT_Mu\d($|\d($|_v\d*))", prescaleRequirement = "True", tag = "MuAll"),
              steps.Trigger.triggerScan( pattern = r"HLT_Ele\d*", prescaleRequirement = "prescale==1", tag = "Ele"),
-             steps.Trigger.triggerScan( pattern = r"HLT_IsoMu\d*", prescaleRequirement = "prescale==1", tag = "IsoMu"),
-             steps.Trigger.triggerScan( pattern = r"HLT_HT\d*U($|_v\d*)", prescaleRequirement = "prescale==1", tag = "HT"),
-             steps.Trigger.triggerScan( pattern = r"HLT_Jet\d*U($|_v\d*)", prescaleRequirement = "prescale==1", tag = "Jet"),
-             steps.Trigger.hltTurnOnHistogrammer( "%sLeadingPt%s"%params["muon"], (50,0,25), "HLT_Mu9",["HLT_Mu%d"%d for d in [5,3]]),
-             steps.Trigger.hltTurnOnHistogrammer( "%sLeadingPt%s"%params["muon"], (50,0,25), "HLT_Mu15_v1",["HLT_Mu%d"%d for d in [9,7,5,3]]),
+             #steps.Trigger.triggerScan( pattern = r"HLT_IsoMu\d*", prescaleRequirement = "prescale==1", tag = "IsoMu"),
+             #steps.Trigger.triggerScan( pattern = r"HLT_HT\d*U($|_v\d*)", prescaleRequirement = "prescale==1", tag = "HT"),
+             #steps.Trigger.triggerScan( pattern = r"HLT_Jet\d*U($|_v\d*)", prescaleRequirement = "prescale==1", tag = "Jet"),
+             #steps.Trigger.hltTurnOnHistogrammer( "%sLeadingPt%s"%pars["muon"], (50,0,25), "HLT_Mu9",["HLT_Mu%d"%d for d in [5,3]]),
+             #steps.Trigger.hltTurnOnHistogrammer( "%sLeadingPt%s"%pars["muon"], (50,0,25), "HLT_Mu15_v1",["HLT_Mu%d"%d for d in [9,7,5,3]]),
              ])
     
     def listOfSampleDictionaries(self) :
-        return [samples.muon,samples.jetmet]
+        return [samples.muon,samples.jetmet,samples.electron]
 
-    def listOfSamples(self,params) :
+    def listOfSamples(self,pars) :
         from samples import specify
         data = specify(# nFilesMax = 32, nEventsMax = 40000,
-                        names = ["Mu.Run2010A-Nov4ReReco.RECO.Jad",
-                                 "Mu.Run2010B-Nov4ReReco.RECO.Jad",
+                        names = [#"Mu.Run2010A-Nov4ReReco.RECO.Jad",
+                                 #"Mu.Run2010B-Nov4ReReco.RECO.Jad",
+                                 "Electron.Run2010B-Nov4ReReco_v1.RECO.Sparrow",
+                                 "EG.Run2010A-Nov4ReReco_v1.RECO.Sparrow"
                                  ])
         return data
 
     def conclude(self) :
         for tag in self.sideBySideAnalysisTags() :
             org=organizer.organizer( self.sampleSpecs(tag) )
-            org.mergeSamples(targetSpec = {"name":"Mu2010A", "color":r.kBlack}, allWithPrefix="Mu.Run2010A")
-            org.mergeSamples(targetSpec = {"name":"Mu2010B", "color":r.kRed}, allWithPrefix="Mu.Run2010B")
+            #org.mergeSamples(targetSpec = {"name":"Mu2010A", "color":r.kBlack}, allWithPrefix="Mu.Run2010A")
+            #org.mergeSamples(targetSpec = {"name":"Mu2010B", "color":r.kRed}, allWithPrefix="Mu.Run2010B")
+            org.mergeSamples(targetSpec = {"name":"EG2010A", "color":r.kBlack}, allWithPrefix="EG")
+            org.mergeSamples(targetSpec = {"name":"Ele2010B", "color":r.kRed}, allWithPrefix="Electron")
             #org.scale()
             
             #plot
