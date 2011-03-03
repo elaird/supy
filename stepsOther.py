1f255bfb9c6101e2941948225e7a07fae9939b4c
bbetchar <bbetchar>
2011-03-03 03:51:52 +0000

    new plots for topAsymm

--- stepsOther.py
+++ stepsOther.py
@@ -542,10 +542,18 @@ class topAsymmetry(analysisStep) :
         self.signedY = "%sSignedRapidity%s"%lepton
         self.relY = "%s%s"%lepton+"RelativeRapiditymixedSumP4Nu"
         self.bins = 31
+        self.TopReco = "%sTopReconstruction%s"%lepton
     def uponAcceptance(self,eV) :
         for charge in ["",["Negative","Positive"][max(0,eV[self.charge][eV[self.index]])]] :
             self.book.fill(eV[self.signedY], self.signedY+charge, self.bins,-5,5, title = "%s;%s;events / bin"%(charge,self.signedY))
             self.book.fill(eV[self.relY], self.relY+charge, self.bins,-5,5, title = "%s;#Delta y;events / bin"%charge)
+
+        topReco = eV[self.TopReco]
+        lepTopM = topReco[0]['lepTopP4'].M()
+        self.book.fill( lepTopM, "bestLepTopM", 100,0,300, title = ";best leptonic top mass;events / bin" )
+        self.book.fill( (lepTopM, topReco[0]['hadTopP4'].M()), "lepM_vs_hadM", (100,100),(0,0),(300,300),
+                        title = ";best leptonic top mass; corresponding hadronic top mass;events / bin",)
+        self.book.fill( topReco[1]['lepTopP4'].M(), "secondBestLepTopM", 100,0,300, title = ";2nd best leptonic top mass;events / bin" )
         #steps.Histos.generic(("%s%s"%lepton+"RelativeRapiditymixedSumP4NuM","%s%s"%lepton+"RelativeRapiditymixedSumP4NuP"),
         #                     (101,101), (-5,-5), (5,5), title = ";#Delta y #nu_{-};#Delta y #nu_{+};events / bin",
         #                     funcString = "lambda x: (x[0],x[1])"),        
