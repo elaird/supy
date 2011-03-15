ac1a2702703daf437cc01a8c5e93601c8dac9837
elaird <elaird>
2011-03-15 10:52:27 +0000

    updates

--- analyses/eventSkim.py
+++ analyses/eventSkim.py
@@ -5,7 +5,10 @@ import os,analysis,utils,calculables,steps,samples
 class eventSkim(analysis.analysis) :
     def listOfSteps(self,params) :
         stepList=[ steps.Print.progressPrinter(2,300),
-                   steps.Other.runLsEventFilter("/home/hep/elaird1/colin.txt"),
+                   steps.Other.runLsEventFilter("/home/hep/elaird1/58_wpol_events/v2/38_Run_event_ls.txt"),
+                   #steps.Other.runLsEventFilter("/home/hep/elaird1/58_wpol_events/v2/38_misRun_event_ls.txt"),
+                   #steps.Other.runLsEventFilter("/home/hep/elaird1/58_wpol_events/v2/39_Run_event_ls.txt"),
+                   #steps.Other.runLsEventFilter("/home/hep/elaird1/58_wpol_events/v2/39_misRun_event_ls.txt"),
                    steps.Other.skimmer(),
                    ]
         return stepList
@@ -15,17 +18,7 @@ class eventSkim(analysis.analysis) :
 
     def listOfSamples(self,params) :
         from samples import specify
-        return [
-            specify(name = "Run2010B_MJ_skim5",         ),
-            specify(name = "Run2010B_MJ_skim4",         ),
-            specify(name = "Run2010B_MJ_skim3",         ),
-            specify(name = "Run2010B_MJ_skim2",         ),
-            specify(name = "Run2010B_MJ_skim",          ),
-            specify(name = "Run2010B_J_skim2",          ),
-            specify(name = "Run2010B_J_skim",           ),
-            specify(name = "Run2010A_JM_skim",          ),
-            specify(name = "Run2010A_JMT_skim",         ),
-            ]
+        return specify(names = "wpol_38" )
 
     def listOfSampleDictionaries(self) :
         return [samples.jetmet,samples.mc]
