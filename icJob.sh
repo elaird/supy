becdc0c1ff31a6a8880684c69a8fa36fb374d829
elaird <elaird>
2011-03-16 16:48:05 +0000

    cmssw 4_1_2

--- icJob.sh
+++ icJob.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 export X509_USER_PROXY=/home/hep/$USER/.MyProxy
-#USERTMP="/vols/cms02/$USER/tmp"
-CMSSW=/vols/sl5_exp_software/cms/slc5_ia32_gcc434/cms/cmssw/CMSSW_3_8_5/src
-#mkdir -p $USERTMP >& /dev/null
+export SCRAM_ARCH=slc5_amd64_gcc434
+CMSSW=/vols/sl5_exp_software/cms/slc5_amd64_gcc434/cms/cmssw/CMSSW_4_1_2/src
 . /vols/cms/grid/setup.sh
 cd $CMSSW && eval `scram runtime -sh` && cd - >& /dev/null
-export LD_LIBRARY_PATH=/vols/cms/grid/dcap:/vols/grid/glite/ui/current/d-cache/dcap/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/vols/cms/grid/dcap:/vols/grid/glite/ui/current/d-cache/dcap/lib:$LD_LIBRARY_PATH
