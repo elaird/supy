013f9a6c146c414d7e60c738ce124b9d93a63fb3
elaird <elaird>
2011-03-20 16:25:59 +0000

    4_1_3

--- icJob.sh
+++ icJob.sh
@@ -1,4 +1,4 @@
 export SCRAM_ARCH=slc5_amd64_gcc434
 source /vols/cms/grid/setup.sh
 export LD_PRELOAD=""
-cd /vols/sl5_exp_software/cms/${SCRAM_ARCH}/cms/cmssw/CMSSW_4_1_2/src && eval `scram runtime -sh` && cd - >& /dev/null
+cd /vols/sl5_exp_software/cms/${SCRAM_ARCH}/cms/cmssw/CMSSW_4_1_3/src && eval `scram runtime -sh` && cd - >& /dev/null
