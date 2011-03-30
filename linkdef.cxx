0eb90c092426b3023c67d3526495ba790f0c7af7
bbetchar <bbetchar>
2011-03-30 04:50:24 +0000

    Support for BoostZ and PtEtaPhiM4D

--- linkdef.cxx
+++ linkdef.cxx
@@ -1,5 +1,6 @@
 #include "Math/LorentzVector.h"
 #include "extendVectorUtil.h"
+#include "Math/BoostZ.h"
 #ifdef __CINT__ 
 typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > LV;
 #pragma link C++ class ROOT::Math::PtEtaPhiM4D<float>+;
@@ -8,6 +9,7 @@ typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > LV;
 //#pragma link C++ class ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> >::*+;
 #pragma link C++ function ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> >::operator+(LV);
 #pragma link C++ function ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> >::operator-(LV);
+#pragma link C++ function ROOT::Math::BoostZ::operator()(LV);
 #pragma link C++ namespace ROOT::Math::VectorUtil+;
 //#pragma link C++ nestedclasses; 
 //#pragma link C++ nestedtypedefs;
