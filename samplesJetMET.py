a69e8d6087cbc9665895fba3bc0b19cf75feb01b
elaird <elaird>
2011-04-23 12:25:32 +0000

    add Arlos 3/pb

--- samplesJetMET.py
+++ samplesJetMET.py
@@ -5,6 +5,8 @@ jetmet = samples.SampleHolder()
 #2011
 jetmet.add("HT.Run2011A-PromptReco-v1.AOD.Georgia",'%s/georgia//ICF/automated/2011_04_09_18_07_10/")'%srm, lumi = 11.88)
 jetmet.add("HT.Run2011A-PromptReco-v1.AOD.Henning",'%s/henning//ICF/automated/2011_04_13_17_17_19/")'%srm, lumi =  5.07)
+jetmet.add("HT.Run2011A-PromptReco-v2.AOD.Arlo", '%s/arlogb//ICF/automated/2011_04_20_15_01_48/")'%srm, lumi = 3.08) #was partially complete
+
 jetmet.add("HT_skim_calo", 'utils.fileListFromDisk(location = "/home/hep/elaird1/73_candidates/v1/calo.root", isDirectory = False)', lumi = 17.0)
 jetmet.add("HT_skim_pf", 'utils.fileListFromDisk(location = "/home/hep/elaird1/73_candidates/v1/pf.root", isDirectory = False)', lumi = 17.0)
 
