da226db42bcc9553b1626930dbb1d5ef31b368a6
elaird <elaird>
2011-04-23 13:42:10 +0000

    skim photon dataset

--- analyses/photonSkim.py
+++ analyses/photonSkim.py
@@ -19,14 +19,15 @@ class photonSkim(analysis.analysis) :
                [calculables.Photon.Indices(collection = params["photon"], ptMin = 80, flagName = "photonIDNoIsoReqPat")]
 
     def listOfSamples(self,params) :
-        from samples import specify        
-        return specify(names = ["HT.Run2011A-PromptReco-v1.AOD.Henning"]) +\
-               specify(names = ["HT.Run2011A-PromptReco-v1.AOD.Georgia"]) +\
-               specify(names = ["qcd_mg_ht_100_250", "qcd_mg_ht_250_500", "qcd_mg_ht_500_1000", "qcd_mg_ht_1000_inf"]) +\
-               specify(names = ["g_jets_mg_ht_40_100", "g_jets_mg_ht_100_200", "g_jets_mg_ht_200_inf"])
+        from samples import specify
+        return specify(names = ["Photon.Run2011A-PromptReco-v1.AOD.Henning1","Photon.Run2011A-PromptReco-v1.AOD.Henning2"])# +\
+               #specify(names = ["HT.Run2011A-PromptReco-v1.AOD.Henning"]) +\
+               #specify(names = ["HT.Run2011A-PromptReco-v1.AOD.Georgia"]) +\
+               #specify(names = ["qcd_mg_ht_100_250", "qcd_mg_ht_250_500", "qcd_mg_ht_500_1000", "qcd_mg_ht_1000_inf"]) +\
+               #specify(names = ["g_jets_mg_ht_40_100", "g_jets_mg_ht_100_200", "g_jets_mg_ht_200_inf"])
 
     def listOfSampleDictionaries(self) :
-        return [samples.jetmet, samples.mc]
+        return [samples.jetmet, samples.photon, samples.mc]
 
     def conclude(self) :
         org = organizer.organizer( self.sampleSpecs() )
