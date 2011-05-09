cfafb50c50ae819194c66b5eb75dbc49d8d4bf6b
elaird <elaird>
2011-05-09 06:26:12 +0000

    new photon data

--- analyses/triggerLook.py
+++ analyses/triggerLook.py
@@ -27,7 +27,7 @@ class triggerLook(analysis.analysis) :
 
     def listOfSamples(self,params) :
         from samples import specify
-        return specify(names = ["HT.Run2011A-PromptReco-v2.AOD.Arlo2"])
+        return specify(names = ["Photon.Run2011A-PromptReco-v2.AOD.Ted2_noIsoReqSkim"])
         
     def conclude(self) :
         for tag in self.sideBySideAnalysisTags() :
