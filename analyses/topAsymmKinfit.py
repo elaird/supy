e7aaad365ea46bc442da840ddfebebeb29b2793e
bbetchar <bbetchar>
2011-05-11 23:01:11 +0000

    update

--- analyses/topAsymmKinfit.py
+++ analyses/topAsymmKinfit.py
@@ -12,7 +12,8 @@ class topAsymmKinfit(topAsymmShell.topAsymmShell) :
     def listOfCalculables(self,pars) :
         calcs = super(topAsymmKinfit,self).listOfCalculables(pars)
         calcs.append( calculables.Top.genTopSemiLeptonicWithinAcceptance( jetPtMin = 20, jetAbsEtaMax=3.5, lepPtMin=21, lepAbsEtaMax = 2.6))
-        calcs.append( calculables.Top.genTopRecoIndex(0.5))
+        calcs.append( calculables.Top.genTopSemiLeptonicAccepted( pars['objects']['jet']))
+        calcs.append( calculables.Top.genTopRecoIndex())
         calcs += [calculables.Jet.TagProbability(pars['objects']['jet'], pars['bVar'], letter) for letter in ['b','q','n']]
         calcs.append( calculables.Top.TopComboLikelihood(pars['objects']['jet'], pars['bVar']))
         return calcs
@@ -28,8 +29,9 @@ class topAsymmKinfit(topAsymmShell.topAsymmShell) :
             steps.Filter.pt("%sP4%s"%lepton, min = lPtMin, indices = "%sIndicesAnyIso%s"%lepton, index = 0),
             ]+topAsymmShell.topAsymmShell.cleanupSteps(pars)+[
             ]+topAsymmShell.topAsymmShell.selectionSteps(pars, withPlots = False) +[
-            steps.Filter.label('kinfit'),  steps.Top.kinFitLook("fitTopRecoIndex"),
+            #steps.Filter.label('kinfit'),  steps.Top.kinFitLook("fitTopRecoIndex"),
             steps.Filter.value("genTopSemiLeptonicWithinAcceptance", min = True),
+            steps.Filter.value("genTopSemiLeptonicAccepted", min = True),
             #]+sum([[steps.Filter.label(tag),steps.Top.jetProbability(obj['jet'], tag,bins,min,max)] \
             #       for tag,bins,min,max in [("JetProbabilityBJetTags",100,-0.2,3),
             #                                ("JetBProbabilityBJetTags",100,-1,12),
@@ -37,6 +39,8 @@ class topAsymmKinfit(topAsymmShell.topAsymmShell) :
             #                                ("CombinedSecondaryVertexMVABJetTags",100,-0.1,1),
             #                       ("TrkCountingHighEffBJetTags",100,-1,15)
             #                                ]],[]) + [
+            steps.Top.topProbLook(obj['jet']),
+            steps.Other.assertNotYetCalculated("TopReconstruction"),
             steps.Filter.value("genTopRecoIndex", min = 0),
             steps.Filter.label('kinfit selected combo'),  steps.Top.kinFitLook("fitTopRecoIndex"),
             steps.Filter.label('kinfit true combo'),  steps.Top.kinFitLook("genTopRecoIndex"),
@@ -64,7 +68,7 @@ class topAsymmKinfit(topAsymmShell.topAsymmShell) :
                                  psFileName = self.psFileName(tag),
                                  doLog = False,
                                  #noSci = True,
-                                 pegMinimum = 0.1,
+                                 #pegMinimum = 0.1,
                                  blackList = ["lumiHisto","xsHisto","nJobsHisto"],
                                  )
             pl.plotAll()
