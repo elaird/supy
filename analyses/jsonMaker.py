21365cbced094c78f2b94ec2c9df98eb6bafc3aa
elaird <elaird>
2011-05-17 17:43:10 +0000

    make jsons

--- analyses/jsonMaker.py
+++ analyses/jsonMaker.py
@@ -16,13 +16,19 @@ class jsonMaker(analysis.analysis) :
         jw = calculables.Other.jsonWeight("/home/hep/elaird1/supy/Cert_160404-163869_7TeV_PromptReco_Collisions11_JSON.txt", acceptFutureRuns = False) #193/pb
         
         out = []
-        out += specify(names = "Photon.Run2011A-PromptReco-v1.AOD.Henning1", weights = jw)
-        out += specify(names = "Photon.Run2011A-PromptReco-v1.AOD.Henning2", weights = jw)
-        out += specify(names = "Photon.Run2011A-PromptReco-v2.AOD.Ted1",     weights = jw)
-        out += specify(names = "Photon.Run2011A-PromptReco-v2.AOD.Ted2",     weights = jw)
-        out += specify(names = "Photon.Run2011A-PromptReco-v2.AOD.Ted3",     weights = jw)
-        #out += specify(names = "HT.Run2011A-PromptReco-v2.AOD.Arlo2",        weights = jw)
-        #out += specify(names = "HT.Run2011A-PromptReco-v2.AOD.Robin1",       weights = jw)
+        #out += specify(names = "Photon.Run2011A-PromptReco-v1.AOD.Henning1", weights = jw)
+        #out += specify(names = "Photon.Run2011A-PromptReco-v1.AOD.Henning2", weights = jw)
+        #out += specify(names = "Photon.Run2011A-PromptReco-v2.AOD.Ted1",     weights = jw)
+        #out += specify(names = "Photon.Run2011A-PromptReco-v2.AOD.Ted2",     weights = jw)
+        #out += specify(names = "Photon.Run2011A-PromptReco-v2.AOD.Ted3",     weights = jw)
+
+        out += specify(names = "HT.Run2011A-PromptReco-v1.AOD.Arlo",     weights = jw)
+        #out += specify(names = "HT.Run2011A-PromptReco-v1.AOD.Zoe",      weights = jw)
+        out += specify(names = "HT.Run2011A-PromptReco-v2.AOD.Arlo",     weights = jw)
+        out += specify(names = "HT.Run2011A-PromptReco-v2.AOD.Arlo2",    weights = jw)
+        out += specify(names = "HT.Run2011A-PromptReco-v2.AOD.Robin1",   weights = jw)
+        #out += specify(names = "HT.Run2011A-PromptReco-v2.AOD.Zoe1",     weights = jw)
+        #out += specify(names = "HT.Run2011A-PromptReco-v2.AOD.Zoe2",     weights = jw)
         return out
 
     def listOfSampleDictionaries(self) :
