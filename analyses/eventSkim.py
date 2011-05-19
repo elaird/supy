0143ef02658f6efc05e44830efd9cbb70d053596
elaird <elaird>
2011-05-19 11:49:50 +0000

    darrens event

--- analyses/eventSkim.py
+++ analyses/eventSkim.py
@@ -5,7 +5,7 @@ import os,analysis,utils,calculables,steps,samples
 class eventSkim(analysis.analysis) :
     def listOfSteps(self,params) :
         stepList=[ steps.Print.progressPrinter(2,300),
-                   steps.Other.runLsEventFilter("/home/hep/elaird1/78_bryn_sync/events.txt"),
+                   steps.Other.runLsEventFilter("/home/hep/elaird1/84_darrens_event/list.txt"),
                    #steps.Other.runLsEventFilter("/home/hep/elaird1/75_rob_sync/v1/robs_events/ht300.txt"),
                    #steps.Other.runLsEventFilter("/home/hep/elaird1/58_wpol_events/v2/38_misRun_event_ls.txt"),
                    #steps.Other.runLsEventFilter("/home/hep/elaird1/58_wpol_events/v2/39_Run_event_ls.txt"),
@@ -19,7 +19,7 @@ class eventSkim(analysis.analysis) :
 
     def listOfSamples(self,params) :
         from samples import specify
-        return specify(names = ["HT.Run2011A-PromptReco-v1.AOD.Georgia","HT.Run2011A-PromptReco-v1.AOD.Henning","HT.Run2011A-PromptReco-v2.AOD.Arlo"] )
+        return specify(names = ["qcd_mg_ht_100_250", "qcd_mg_ht_250_500", "qcd_mg_ht_500_1000", "qcd_mg_ht_1000_inf"] )
 
     def listOfSampleDictionaries(self) :
         return [samples.jetmet,samples.mc]
