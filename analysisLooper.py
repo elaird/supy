cab8a735cbb9124620844497caec2ca81e7cca37
bbetchar <bbetchar>
2011-06-04 21:31:21 +0000

    rearranging and cleanup; scarier than it looks, but tested

--- analysisLooper.py
+++ analysisLooper.py
@@ -10,71 +10,86 @@ class analysisLooper :
                  localStem = None, globalStem = None, subDir = None, steps = None, calculables = None, inputFiles = None, name = None,
                  nEventsMax = None, quietMode = None, color = None, markerStyle = None) :
 
+        for arg in ["steps", "calculables"] : setattr(self, arg, eval("copy.deepcopy(%s)"%arg))
         for arg in ["fileDirectory", "treeName", "otherTreesToKeepWhenSkimming", "leavesToBlackList",
                     "localStem", "globalStem", "subDir", "inputFiles", "name", "nEventsMax", "quietMode", "color", "markerStyle"] :
             setattr(self, arg, eval(arg))
 
-        for arg in ["steps", "calculables"] :
-            setattr(self, arg, eval("copy.deepcopy(%s)"%arg))
-
-        self.globalDir = "%s/%s/"%(self.globalStem, self.subDir)
         self.outputDir = self.globalDir #the value will be modified in self.prepareOutputDirectory()
         self.checkSteps()
 
-    def childName(self, iSlice) :
-        return "%s_%d"%(self.name, iSlice)
+    @property
+    def mainTree(self) : return (self.fileDirectory,self.treeName)
+    @property
+    def globalDir(self) : return "%s/%s/"%(self.globalStem, self.subDir)
+    @property
+    def outputFileStem(self) :  return "%s/%s"%(self.outputDir, self.name)
+    @property
+    def pickleFileName(self) : return "%s%s"%(self.outputFileStem, ".pickledData")
 
+    def checkSteps(self) :
+        for iStep,step in enumerate(self.steps) :
+            if iStep : continue
+            assert step.name()=="Master", "The master step must occur first."
+            assert step.isSelector, "The master step must be a selector."
+
+    def childName(self, iSlice) : return "%s_%d"%(self.name, iSlice)
     def slice(self, iSlice, nSlices) :
         out = copy.deepcopy(self)
         out.inputFiles = out.inputFiles[iSlice::nSlices]
         out.name = self.childName(iSlice)
         return out
 
-    def outputFileStem(self) :
-        return "%s/%s"%(self.outputDir, self.name)
-
-    def outputStepAndCalculableDataFileName(self) :
-        return "%s%s"%(self.outputFileStem(), ".pickledData")
-
-    def checkSteps(self) :
-        for iStep,step in enumerate(self.steps) :
-            if step.name()=="Master" :
-                assert not iStep,"The master step must occur first."
-                assert hasattr(step,"select"), "The master step must be a selector."
-            else :
-                assert iStep,"The master step must occur first."
         
     def go(self) :
         self.prepareOutputDirectory()
-        
-        self.setupChains(self.inputFiles)
-        useSetBranchAddress = self.setupSteps()
+        self.setupChains()
+        self.setupSteps()
+        self.loop()
+        self.endSteps()
+        self.writePickle()
+        self.writeRoot()
+        self.deleteChains()
+        self.moveFiles()
+        if not self.quietMode : print utils.hyphens
 
-        #loop through entries
+    def loop(self) :
         if self.nEventsMax!=0 :
-            chainWrapper = wrappedChain.wrappedChain(self.inputChain,
-                                                     calculables = self.calculables,
-                                                     useSetBranchAddress = useSetBranchAddress,
-                                                     leavesToBlackList = self.leavesToBlackList,
-                                                     maxArrayLength = configuration.maxArrayLength(),
-                                                     trace = configuration.trace(),
-                                                     )
+            chainWrapper = wrappedChain.wrappedChain( self.chains[self.mainTree],
+                                                      calculables = self.calculables,
+                                                      useSetBranchAddress = not any([step.requiresNoSetBranchAddress() for step in self.steps]),
+                                                      leavesToBlackList = self.leavesToBlackList,
+                                                      maxArrayLength = configuration.maxArrayLength(),
+                                                      trace = configuration.trace(),
+                                                      )
             map( self.processEvent, chainWrapper.entries(self.nEventsMax) )
             self.makeListsOfLeavesAndCalcsUsed( chainWrapper.activeKeys(), chainWrapper.calcDependencies() )
         else : self.makeListsOfLeavesAndCalcsUsed([], {})
 
-        self.endSteps()
-        self.pickleStepAndCalculableData()
-        self.writeHistos()
-        if not self.quietMode : print utils.hyphens
+    def processEvent(self,eventVars) :
+        for step in self.steps :
+            try:
+                if not step.go(eventVars) : break
+            except Exception as e:
+                print "\nProblem with %s\n%s\n"%(type(step),step.moreName)
+                import traceback
+                tb = sys.exc_info()[2]
+                traceback.print_tb(tb, limit=20, file=sys.stdout)
+                print e.__class__.__name__,":", e
+                sys.exit(0)
+
+    def makeListsOfLeavesAndCalcsUsed(self, activeKeys, calculableDependencies) :
+        calcs = dict([(calc.name(),calc) for calc in self.calculables])
+        self.calculableDependencies = calculableDependencies
+        self.listOfLeavesUsed = []
+        self.listOfCalculablesUsed = []
+        for key,isLeaf,keyType in activeKeys :
+            if isLeaf : self.listOfLeavesUsed.append((key, keyType))
+            else : self.listOfCalculablesUsed.append( (key, "%s%s%s" % (calcs[key].moreName, calcs[key].moreName2,
+                                                                        configuration.fakeString() if calcs[key].isFake() else "") ) )        
+        self.listOfLeavesUsed.sort()
+        self.listOfCalculablesUsed.sort()
         
-        #free up memory (http://wlav.web.cern.ch/wlav/pyroot/memory.html)
-        self.inputChain.IsA().Destructor( self.inputChain )
-        for chain in self.otherChainDict.values() :
-            chain.IsA().Destructor( chain )
-
-        self.moveFiles()
-
     def prepareOutputDirectory(self) :
         utils.mkdir(self.localStem)
         self.tmpDir = tempfile.mkdtemp(dir = self.localStem)
@@ -86,104 +101,91 @@ class analysisLooper :
         os.system("rsync -a %s/ %s/"%(self.outputDir, self.globalDir))
         os.system("rm -r %s"%self.tmpDir)
         
-    def processEvent(self,eventVars) :
-        for step in self.steps :
-            try:
-                if not step.go(eventVars) : break
-            except Exception as e:
-                print
-                print "Problem with %s"%type(step)
-                print step.moreName
-                print
-                import traceback
-                tb = sys.exc_info()[2]
-                traceback.print_tb(tb, limit=10, file=sys.stdout)
-                print e.__class__.__name__,":", e
-                sys.exit(0)
-        
-    def setupChains(self,inputFiles) :
-        nFiles = len(inputFiles)
-        alreadyPrintedEllipsis = False
+    def setupChains(self) :
+        self.chains = dict( [(item,r.TChain("chain%d"%iItem)) for iItem,item in enumerate([self.mainTree]+self.otherTreesToKeepWhenSkimming)])
+        for (dirName,treeName),chain in self.chains.iteritems() :
+            for infile in self.inputFiles : chain.Add("%s/%s/%s"%(infile, dirName, treeName))
+            r.SetOwnership(chain, False)
 
         if not self.quietMode :
+            nFiles = len(self.inputFiles)
+            nEventsString = str(self.chains[self.mainTree].GetEntries()) if configuration.computeEntriesForReport() else "(number not computed)"
             print utils.hyphens
-            print "The %d \"%s\" input file%s:"%(nFiles, self.name, "s" if nFiles>1 else "")
-
-        self.inputChain = r.TChain("chain")
-        r.SetOwnership(self.inputChain, False)
-        
-        self.otherChainDict = {}
-        otherChainCount = 0
-        for item in self.otherTreesToKeepWhenSkimming :
-            self.otherChainDict[item] = r.TChain("chain%d"%otherChainCount)
-            r.SetOwnership(self.otherChainDict[item], False)
-        
-        for infile in inputFiles :
-            #add main tree to main chain
-            self.inputChain.Add("%s/%s/%s"%(infile, self.fileDirectory, self.treeName))
-            
-            #add other trees to other chains
-            for (dirName,treeName),chain in self.otherChainDict.iteritems() :
-                chain.Add("%s/%s/%s"%(infile, dirName, treeName))
-            if (inputFiles.index(infile)<2 or inputFiles.index(infile)>(nFiles-3) ) :
-                if not self.quietMode : print infile
-            elif (not alreadyPrintedEllipsis) :
-                if not self.quietMode : print "..."
-                alreadyPrintedEllipsis = True
-
-        outString = "contain%s %s events."%(("s" if nFiles==1 else ""),
-                                            str(self.inputChain.GetEntries()) if configuration.computeEntriesForReport() else "(number not computed)",
-                                            )
-        if not self.quietMode :
-            print outString
+            print "The %d \"%s\" input file%s:"%(nFiles, self.name, "s" if nFiles>1 else '')
+            print '\n'.join(self.inputFiles[:2]+["..."]+self.inputFiles[2:][-2:])
+            print "contain%s %s events."%(("s" if nFiles==1 else ""), nEventsString)
             print utils.hyphens
-        r.gROOT.cd()
+
+    def deleteChains(self) : #free up memory (http://wlav.web.cern.ch/wlav/pyroot/memory.html)
+        for chain in self.chains.values() : chain.IsA().Destructor( chain )        
+
 
     def setupSteps(self, minimal = False) :
-        returnValue = True
         r.gROOT.cd()
         current = r.gDirectory
         priorFilters = []
+        self.steps[0].books = []
 
         for step in self.steps :
-            step.isSelector = hasattr(step,"select")            
-            if step.isSelector : priorFilters.append((step.name(),step.moreName+step.moreName2))
-            step.setOutputFileStem(self.outputFileStem())
+            step.setOutputFileStem(self.outputFileStem)
             current = current.mkdir(step.name())
             step.book = autoBook(current)
             step.tracer = wrappedChain.keyTracer(None) if configuration.trace() else None
             step.priorFilters = set(priorFilters)
 
+            self.steps[0].books.append(step.book)
+            if step.isSelector : priorFilters.append((step.name(),step.moreName+step.moreName2))
+
             if minimal : continue
             if self.quietMode : step.makeQuiet()
             assert step.isSelector ^ hasattr(step,"uponAcceptance"), "Step %s must implement 1 and only 1 of {select,uponAcceptance}"%step.name()
-            if step.requiresNoSetBranchAddress() : returnValue = False
-            step.setup(self.inputChain, self.fileDirectory)
+            step.setup(self.chains[self.mainTree], self.fileDirectory)
 
-        #make Master aware of all steps' books
-        self.steps[0].books = [step.book for step in self.steps]
-
-        r.gROOT.cd()
-        return returnValue
-
-    def makeListsOfLeavesAndCalcsUsed(self, activeKeys, calculableDependencies) :
-        self.calculableDependencies = calculableDependencies
-        self.listOfLeavesUsed = []
-        self.listOfCalculablesUsed = []
-        for key,isLeaf,keyType in activeKeys :
-            if isLeaf :
-                self.listOfLeavesUsed.append((key, keyType))
-            else :
-                for calc in self.calculables :
-                    if calc.name()!=key : continue
-                    self.listOfCalculablesUsed.append( (calc.name(), "%s%s%s" % \
-                                                        (calc.moreName if hasattr(calc,"moreName") else "",\
-                                                         calc.moreName2 if hasattr(calc,"moreName2") else "",\
-                                                         configuration.fakeString() if calc.isFake() else "") ) )
+    def endSteps(self) : [ step.endFunc(self.chains) for step in self.steps ]
         
-        self.listOfLeavesUsed.sort()
-        self.listOfCalculablesUsed.sort()
-        
+    def writePickle(self) :
+        def pickleJar(step) :
+            inter = set(step.varsToPickle()).intersection(set(['nPass','nFail','outputFileName']))
+            assert not inter, "%s is trying to pickle %s, which %s reserved for use by analysisStep."%(step.name(), str(inter), "is" if len(inter)==1 else "are")
+            return dict([ (item, getattr(step,item)) for item in step.varsToPickle()+['nPass','nFail']] +
+                        [('outputFileName', getattr(step,'outputFileName').replace(self.outputDir, self.globalDir))])
+
+        outFile = open(self.pickleFileName, "w")
+        cPickle.dump([ [pickleJar(step) for step in self.steps], self.listOfCalculablesUsed, self.listOfLeavesUsed], outFile)
+        outFile.close()
+
+    def writeRoot(self) :
+        def writeFromSteps() :
+            while "/" not in r.gDirectory.GetName() : r.gDirectory.GetMotherDir().cd()
+            for step in self.steps :
+                r.gDirectory.mkdir(step.book.title, step.moreName+step.moreName2).cd()
+                for hist in [step.book[name] for name in step.book.fillOrder] :
+                    hist.Write()
+                    hist.Delete()
+                if configuration.trace() and step.isSelector :
+                    r.gDirectory.mkdir("Calculables").cd()
+                    for key in step.tracer.tracedKeys : r.gDirectory.mkdir(key)
+                    r.gDirectory.GetMother().cd()
+    
+        def writeNodesUsed() :
+            while "/" not in r.gDirectory.GetName() : r.gDirectory.GetMotherDir().cd()
+            r.gDirectory.mkdir("Leaves",".").cd()
+            for leaf in self.listOfLeavesUsed: r.gDirectory.mkdir(*leaf)
+            r.gDirectory.GetMother().cd()
+            r.gDirectory.mkdir("Calculables",".").cd()
+            for calc in self.listOfCalculablesUsed :
+                r.gDirectory.mkdir(*calc).cd()
+                for dep in self.calculableDependencies[calc[0]] : r.gDirectory.mkdir(dep)
+                r.gDirectory.GetMother().cd()
+            r.gDirectory.GetMother().cd()
+
+        outputFile = r.TFile(self.steps[0].outputFileName, "RECREATE")
+        writeNodesUsed()
+        writeFromSteps()
+        outputFile.Close()
+
+
+
     def printStats(self) :
         print utils.hyphens
         print self.name
@@ -209,70 +211,5 @@ class analysisLooper :
         for step in self.steps :
             step.printStatistics()
         print utils.hyphens
-        
-    def writeHistosFromBooks(self) :
-        while "/" not in r.gDirectory.GetName() : r.gDirectory.GetMotherDir().cd()
-        wroteSlash = False
-        for step in self.steps :
-            name = step.book._autoBook__directory.GetName()
-            if '/' in name :
-                if wroteSlash: continue
-                wroteSlash = True
-            else:
-                r.gDirectory.mkdir(name,step.moreName+step.moreName2).cd()
-                if configuration.trace() and step.isSelector :
-                    r.gDirectory.mkdir("Calculables").cd()
-                    for key in step.tracer.tracedKeys : r.gDirectory.mkdir(key)
-                    r.gDirectory.GetMother().cd()
-            
-            for item in step.book.fillOrder :
-                object = step.book[item]
-                object.Write()
-                object.Delete()
-        while "/" not in r.gDirectory.GetName() : r.gDirectory.GetMotherDir().cd()
 
-    def writeNodesUsed(self) :
-        while "/" not in r.gDirectory.GetName() : r.gDirectory.GetMotherDir().cd()
-        r.gDirectory.mkdir("Leaves",".").cd()
-        for leaf in self.listOfLeavesUsed: r.gDirectory.mkdir(*leaf)
-        r.gDirectory.GetMother().cd()
-        r.gDirectory.mkdir("Calculables",".").cd()
-        for calc in self.listOfCalculablesUsed :
-            r.gDirectory.mkdir(*calc).cd()
-            for dep in self.calculableDependencies[calc[0]] : r.gDirectory.mkdir(dep)
-            r.gDirectory.GetMother().cd()
-        r.gDirectory.GetMother().cd()
-
-    def writeHistos(self) :
-        outputFile = r.TFile(self.steps[0].outputFileName(), "RECREATE")
-        self.writeNodesUsed()
-        self.writeHistosFromBooks()
-        outputFile.Close()
-
-    def endSteps(self) :
-        for step in self.steps :
-            step.endFunc(self.otherChainDict)
-
-    def pickleStepAndCalculableData(self) :
-        def listToDump() :
-            out = []
-            for step in self.steps :
-                d = {}
-                vars = set(step.varsToPickle())
-                items = ["nPass", "nFail", "outputFileName"]
-                inter = vars.intersection(set(items))
-                assert not inter, "Variables to be pickled cannot be called anything in %s; %s is trying to pickle %s."%(str(items), step.name(), str(inter))
-                for item in vars :
-                    d[item] = getattr(step, item)
-                for item in items :
-                    if item=="outputFileName" :
-                        d[item] = getattr(step, item)().replace(self.outputDir, self.globalDir)
-                    else :
-                        d[item] = getattr(step, item)()
-                out.append(d)
-            return out
-            
-        outFile = open(self.outputStepAndCalculableDataFileName(), "w")
-        cPickle.dump([listToDump(), self.listOfCalculablesUsed, self.listOfLeavesUsed], outFile)
-        outFile.close()
 #####################################
