3eac36dc04db66b7e3f3ea583b454cc107ca4427
bbetchar <bbetchar>
2011-06-24 16:54:26 +0000

    07_prescaleWeight

--- analyses/prescales2.py
+++ analyses/prescales2.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python
-
 import os,analysis,steps,calculables,samples,organizer,plotter,utils
 import ROOT as r
 
 class prescales2(analysis.analysis) :
     def mutriggers(self) :
-        ptv = {   3:(3,4),
-                  5:(3,4,5,6),
-                  8:(1,2,3,4),
+        ptv = {   #3:(3,4),
+                  #5:(3,4,5,6),
+                  #8:(1,2,3,4),
                  12:(1,2,3,4),
                  15:(2,3,4,5),
                  20:(1,2,3,4),
                  24:(1,2,3,4),
                  30:(1,2,3,4),
                  40:(1,2),
-                100:(1,2) }
+                 #100:(1,2),
+                 }
         return sum([[("HLT_Mu%d_v%d"%(pt,v),pt+1) for v in vs] for pt,vs in sorted(ptv.iteritems())],[])
 
     def parameters(self) :
@@ -41,6 +40,7 @@ class prescales2(analysis.analysis) :
             steps.Filter.monster(),
             steps.Filter.hbheNoise(),
             steps.Filter.value("%sTriggeringPt%s"%pars["muon"],min=10),
+            steps.Trigger.prescaleLumiEpochs(pars['triggers']),
             steps.Trigger.anyTrigger(zip(*pars['triggers'])[0]),
             steps.Histos.value("%sTriggeringPt%s"%pars["muon"],100,0,200),
             steps.Trigger.hltPrescaleHistogrammer(zip(*pars["triggers"])[0]),
@@ -53,29 +53,35 @@ class prescales2(analysis.analysis) :
     def listOfSampleDictionaries(self) : return [samples.muon]
 
     def listOfSamples(self,params) :
-        from samples import specify
-        return ( specify(names = "SingleMu.Run2011A-PR-v4.FJ.Burt", color = r.kBlack ) +
-                 specify(names = "SingleMu.Run2011A-May10-v1.FJ.Burt", color = r.kRed) +
-                 []
-                 )
-        
+        return ( samples.specify( names = "SingleMu.Run2011A-PR-v4.FJ.Burt2", color = r.kBlack) +
+                 samples.specify( names = "SingleMu.Run2011A-PR-v4.FJ.Burt", color = r.kRed) +
+                 samples.specify( names = "SingleMu.Run2011A-May10-v1.FJ.Burt", color = r.kBlue) )
+    
     def conclude(self,pars) :
+        import re
         org = self.organizer(pars)
-        org.mergeSamples(targetSpec = {"name":"SingleMu"}, allWithPrefix="SingleMu")
-        pl = plotter.plotter(org,
-                             psFileName = self.psFileName(org.tag),
-                             blackList = ["lumiHisto","xsHisto","nJobsHisto"],
-                             detailedCalculables = True
-                             )
-        pl.plotAll()
+        black = sum([[hist for hist in step if re.match(r"HLT_Mu\d*_v\d*_p\d*",hist)] for step in org.steps],[])
 
-        print "\t     ptMin\t    prescale  observed\t\tN pass"
-        for step in org.steps :
-            if step.name != "prescaleScan" : continue
-            for hist in sorted(step,key = lambda h: int(h.split("_")[3][1:])) :
-                if not step[hist][0].GetBinContent(2) : continue
-                label =''.join(part.strip('p').rjust(8) for part in hist.split('_'))
-                listed = int(hist.split("_")[3][1:])
-                observed = step[hist][0].GetEntries() / step[hist][0].GetBinContent(2)
-                print label, ("%.1f" % observed).ljust(10), ("%.1f"%(observed/listed)).rjust(7), ("%d"%step[hist][0].GetBinContent(2)).rjust(20)
+        
+        args = {"blackList":["lumiHisto","xsHisto","nJobsHisto"] + black,
+                "detailedCalculables" : True }
+
+        plotter.plotter(org, psFileName = self.psFileName(org.tag+"unmerged"), **args ).plotAll()
+        plotter.plotter(org, psFileName = self.psFileName(org.tag+"unmerged_nolog"), doLog=False, **args ).plotAll()
+        org.mergeSamples(targetSpec = {"name":"SingleMu","color":r.kRed}, allWithPrefix="SingleMu")
+        plotter.plotter(org, psFileName = self.psFileName(org.tag), **args ).plotAll()
+        self.printPrescales(org)
+
                 
+    def printPrescales(self,org) :
+        with open('output.txt','w') as file :
+            print >> file, "\t     ptMin\t    prescale  observed\t\tN pass"
+            for step in org.steps :
+                if step.name != "prescaleScan" : continue
+                for hist in sorted(step,key = lambda h: int(h.split("_")[3][1:])) :
+                    if not step[hist][0].GetBinContent(2) : continue
+                    label =''.join(part.strip('p').rjust(8) for part in hist.split('_'))
+                    listed = int(hist.split("_")[3][1:])
+                    observed = step[hist][0].GetEntries() / step[hist][0].GetBinContent(2)
+                    print >> file, label, ("%.1f" % observed).ljust(10), ("%.1f"%(observed/listed)).rjust(7), ("%d"%step[hist][0].GetBinContent(2)).rjust(20)
+
