cae6498aaa28d864a98af523fe7f3c6541b120cf
elaird <elaird>
2011-06-25 16:58:19 +0000

    make some jsons

--- analyses/jsonMaker.py
+++ analyses/jsonMaker.py
@@ -11,18 +11,25 @@ class jsonMaker(analysis.analysis) :
 
     def listOfSamples(self,params) :
         from samples import specify
-        jw = calculables.Other.jsonWeight("/home/hep/elaird1/supy/Cert_160404-166502_7TeV_PromptReco_Collisions11_JSON.txt", acceptFutureRuns = False) #485/pb        
+        jw = calculables.Other.jsonWeight("/home/hep/elaird1/supy/Cert_160404-167151_7TeV_PromptReco_Collisions11_JSON.txt") #869/pb        
         
         out = []
         #out += specify(names = "Photon.Run2011A-May10ReReco-v1.AOD.Henning.L1", weights = jw)
         #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Ted1.L1",     weights = jw)
         #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Ted2.L1",     weights = jw)
+
+        out += specify(names = "Photon.Run2011A-May10ReReco-v1.AOD.Zoe_skim", weights = jw)
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Zoe1_skim", weights = jw)
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Zoe2_skim", weights = jw)
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Zoe3_skim", weights = jw)
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Rob1",     weights = jw)
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Rob2",     weights = jw)
         
-        out += specify(names = [ "SingleMu.Run2011A-May10-v1.FJ.Burt",
-                                 "SingleMu.Run2011A-PR-v4.FJ.Burt1",
-                                 "SingleMu.Run2011A-PR-v4.FJ.Burt2",
-                                 "SingleMu.Run2011A-PR-v4.FJ.Burt3",
-                                 ]) # no json filtering necessary, golden json used
+        #out += specify(names = [ "SingleMu.Run2011A-May10-v1.FJ.Burt",
+        #                         "SingleMu.Run2011A-PR-v4.FJ.Burt1",
+        #                         "SingleMu.Run2011A-PR-v4.FJ.Burt2",
+        #                         "SingleMu.Run2011A-PR-v4.FJ.Burt3",
+        #                         ]) # no json filtering necessary, golden json used
         return out
 
     def listOfSampleDictionaries(self) :
