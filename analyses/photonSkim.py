bc36ffe07cd5655508a80ccec44a226bc9dfaef8
elaird <elaird>
2011-06-25 21:20:55 +0000

    tight iso requirements and samples

--- analyses/photonSkim.py
+++ analyses/photonSkim.py
@@ -16,7 +16,7 @@ class photonSkim(analysis.analysis) :
     def listOfCalculables(self,params) :
         return calculables.zeroArgs() +\
                calculables.fromCollections(calculables.Photon,[params["photon"]]) +\
-               [calculables.Photon.Indices(collection = params["photon"], ptMin = 100, flagName = "photonIDNoIsoReqPat")]
+               [calculables.Photon.Indices(collection = params["photon"], ptMin = 100, flagName = "photonIDLooseFromTwikiPat")]
 
     def qcdPyNames(self) :
         l = ["80", "120", "170", "300", "470", "600", "800"]
@@ -26,6 +26,14 @@ class photonSkim(analysis.analysis) :
         l = ["80", "120", "170", "300", "470", "800"]
         return ["g_jets_py6_pt_%s_%s"%(a,b) for a,b in zip(l[:-1], l[1:])]
 
+    def qcdMgNames(self) :
+        l = ["100", "250", "500", "1000", "inf"]
+        return ["qcd_mg_ht_%s_%s"%(a,b) for a,b in zip(l[:-1], l[1:])]
+
+    def gJetsMgNames(self) :
+        l = ["40", "100", "200", "inf"]
+        return ["g_jets_mg_ht_%s_%s"%(a,b) for a,b in zip(l[:-1], l[1:])]
+
     def listOfSamples(self,params) :
         from samples import specify
         out = []
@@ -34,13 +42,16 @@ class photonSkim(analysis.analysis) :
         #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Ted1.L1"    )
         #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Ted2.L1"    )
 
+        out += specify(names = "Photon.Run2011A-May10ReReco-v1.AOD.Zoe")
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Zoe1")
         out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Zoe2")
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Zoe3")
         out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Rob1")
         out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Rob2")
-        
-        #out += specify(names = "g_jets_mg_ht_40_100.L1" )
-        #out += specify(names = "g_jets_mg_ht_100_200.L1")
-        #out += specify(names = "g_jets_mg_ht_200_inf.L1")
+
+        out += specify(names = self.qcdMgNames())
+        out += specify(names = self.gJetsMgNames())
+
         return out
     
         #return specify(names = "tt_tauola_mg") +\
