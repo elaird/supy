7ddf645d868711c934cc679217f11fbe5b3f818f
bbetchar <bbetchar>
2011-08-05 17:38:36 +0000

    revamped args and syntax for Ratio

--- analyses/exampleReweight.py
+++ analyses/exampleReweight.py
@@ -2,7 +2,6 @@ import os,math,ROOT as r
 import analysis,samples,calculables
 
 class exampleReweight(analysis.analysis) :
-    def parameters(self) : return {}
 
     def listOfSteps(self,pars) :
         import steps
@@ -12,7 +11,9 @@ class exampleReweight(analysis.analysis) :
                  steps.Filter.multiplicity("vertexIndices",min=1),
                  steps.Filter.pt("muonP4PF", min = 25, indices = "muonIndicesPF", index=0),
                  steps.Histos.multiplicity("vertexIndices",max=15),
-                 calculables.Other.Ratio("nVertex", binning = (15,-0.5,14.5), targetPrefix = "SingleMu", groupPrefixes = ['qcd_mg','qcd_py6'], thisSample = pars['baseSample']),
+                 calculables.Other.Ratio("nVertex", binning = (15,-0.5,14.5), thisSample = pars['baseSample'],
+                                         target = ("SingleMu",[]), groups = [('qcd_mg',[]),('qcd_py6',[])],
+                                         ),
                  steps.Histos.multiplicity("vertexIndices",max=15),
                  ]
     
@@ -28,17 +29,16 @@ class exampleReweight(analysis.analysis) :
     def listOfSampleDictionaries(self) : return [samples.mc,samples.muon]
 
     def listOfSamples(self,pars) :
+        def qcd_mg(eL) :
+            qM = ["%d"%t for t in [50,100,250,500,1000][1:]]
+            return samples.specify( effectiveLumi = eL, weights = "nVertexRatio",
+                                    names = ["qcd_mg_ht_%s_%s"%t for t in zip(qM,qM[1:]+["inf"])])
         def qcd_py6_mu(eL) :
             q6 = [0,5,15,20,30,50,80,120,150,None]
             iCut = q6.index(15)
-            return samples.specify( effectiveLumi = eL, color = r.kOrange, weights = "nVertexRatio",
-                                    names = ["qcd_py6fjmu_pt_%s"%("%d_%d"%(low,high) if high else "%d"%low) for low,high in zip(q6[:-1],q6[1:])[iCut:]] )
-
-        def qcd_mg(eL) :
-            qM = ["%d"%t for t in [50,100,250,500,1000][1:]]
-            return samples.specify( effectiveLumi = eL, color = r.kBlue, weights = "nVertexRatio",
-                                    names = ["qcd_mg_ht_%s_%s"%t for t in zip(qM,qM[1:]+["inf"])])
-
+            return samples.specify( effectiveLumi = eL, weights = "nVertexRatio",
+                                    names = ["qcd_py6fjmu_pt_%s"%("%d_%d"%(low,high) if high else "%d"%low)
+                                             for low,high in zip(q6[:-1],q6[1:])[iCut:]] )
         return (samples.specify(names="SingleMu.Run2011A-PR-v4.FJ.Burt4") +
                 qcd_mg(100) +
                 qcd_py6_mu(100) +
@@ -49,11 +49,10 @@ class exampleReweight(analysis.analysis) :
         org = self.organizer(pars)
         org.mergeSamples(targetSpec = {"name":"qcd_mg", "color":r.kBlue}, allWithPrefix="qcd_mg")
         org.mergeSamples(targetSpec = {"name":"qcd_py6", "color":r.kRed}, allWithPrefix="qcd_py6")
-        org.scale(100)
+        org.scale()
         plotter.plotter( org,
                          psFileName = self.psFileName(org.tag),
                          blackList = ["lumiHisto","xsHisto","xsPostWeightsHisto","nJobsHisto","genpthat"],
                          detailedCalculables = True,
                          ).plotAll()
-
-
+        
--- calculablesOther.py
+++ calculablesOther.py
@@ -226,35 +226,39 @@ class Covariance(wrappedChain.calculable) :
                                [self.source[self.SigmaXY],self.source[self.SigmaYY]]])
 #####################################
 class Ratio(calculables.secondary) :
-    def __init__(self, var=None, binning=(0,0,0), thisSample = None, targetPrefix = None, groupPrefixes = []) :
+    def __init__(self, var=None, binning=(0,0,0), thisSample = None, target = ("",[]), groups = []) :
         self.fixes = (var,"")
         self.defaultValue = 1.0
-        self.groups = [targetPrefix] + groupPrefixes
-        self.thisGroup = next((gp for gp in self.groups if gp in thisSample),None)
-        for item in ["var","binning","groupPrefixes"] : setattr(self,item,eval(item))
+        for item in ["var","binning","target","groups"] : setattr(self,item,eval(item))
+
+        self.thisGroup = next((gp for pre,samples in groups if thisSample in samples),
+                              next((pre for pre,samples in groups if not samples and thisSample.find(pre)==0),
+                                   None))
+        if ( self.thisGroup==None
+             and thisSample not in target[1]
+             and thisSample.find(target[0])!=0 ) : print "Ratio cannot find group for : " + thisSample
 
     def setup(self,*_) :
-        name = 'unweighted'+self.var
-        hists = self.fromCache( [self.groups[0], self.thisGroup], [name])
-        source = hists[self.thisGroup][name]
-        self.weights = hists[self.groups[0]][name]
+        hists = self.fromCache( [self.target[0], self.thisGroup], ['unweighted'])
+        source = hists[self.thisGroup]['unweighted']
+        self.weights = hists[self.target[0]]['unweighted']
         if source and self.weights :
             self.weights.Scale(1./self.weights.Integral(0,self.weights.GetNbinsX()+1))
             source.Scale(1./source.Integral(0,source.GetNbinsX()+1))
             self.weights.Divide(source)
         else : self.weights = None
         
-    def uponAcceptance(self,eventVars) :
-        myWeight = eventVars[self.name]
-        value = eventVars[self.var]
-        self.book.fill( value, "unweighted"+self.var, *self.binning, w = 1, title = ";%s;events / bin"%self.var )
-        self.book.fill( value, "myweighted"+self.var, *self.binning, w = myWeight, title = ";%s;events / bin"%self.var)
-        self.book.fill( value, self.var, *self.binning, title = ";%s;events / bin"%self.var )
-        self.book.fill(math.log(myWeight), "logMyWeight", 40, -5, 5, w = 1, title = ";log(%s);events / bin"%self.name)
+    def uponAcceptance(self,ev) :
+        me = ev[self.name]
+        self.book.fill( ev[self.var], "unweighted", *self.binning, w = 1,  title = ";%s;events / bin"%self.var )
+        self.book.fill( ev[self.var], "meweighted", *self.binning, w = me, title = ";%s;events / bin"%self.var )
+        self.book.fill( math.log(me), "logMyValue", 40, -5, 5,     w = 1,  title = ";log(%s);events / bin"%self.name )
 
     def update(self, ignored) :
         self.value = self.defaultValue if not self.weights else self.weights.GetBinContent(self.weights.FindFixBin(self.source[self.var]))
 
     def organize(self,org) :
-        [org.mergeSamples( targetSpec = {"name":pre}, allWithPrefix=pre) for pre in self.groups]
+        [ org.mergeSamples( targetSpec = {"name":pre}, sources = samples ) if samples else
+          org.mergeSamples( targetSpec = {"name":pre}, allWithPrefix = pre )
+         for pre,samples in self.groups + [self.target] ]
 #####################################
