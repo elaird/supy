ce12d64331a227273f6d17cc48924175bb136a00
bbetchar <bbetchar>
2011-08-09 03:39:45 +0000

    phaddy is ridiculously fast...orders of magnitude improvement on hadd in case of many sources

--- stepsMaster.py
+++ stepsMaster.py
@@ -36,7 +36,7 @@ class Master(analysisStep) :
             for fileName in files : os.remove(fileName)
 
         if not all(os.path.exists(fileName) for fileName in products["outputFileName"]) : return
-        hAdd = utils.getCommandOutput("hadd -f %s %s"%(self.outputFileName, " ".join(products["outputFileName"])))
+        hAdd = utils.getCommandOutput("./phaddy -f %s %s"%(self.outputFileName, " ".join(products["outputFileName"])))
         
         printComment(hAdd["stdout"])
         cleanUp(hAdd["stderr"], products["outputFileName"])
