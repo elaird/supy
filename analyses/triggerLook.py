171f0d5a9a5b2043e335657bf8397b0a256a7713
Edward Laird <elaird@gmail.com>
2011-10-21 12:00:02 +0100

    welcome to the future

--- analyses/triggerLook.py
+++ analyses/triggerLook.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import os,analysis,steps,calculables,samples,organizer,plotter,utils
+from core import analysis,organizer,plotter
+import steps,calculables,samples
 import ROOT as r
 
 class triggerLook(analysis.analysis) :
@@ -9,26 +10,24 @@ class triggerLook(analysis.analysis) :
 
     def listOfSteps(self,params) :
         return [
-            steps.Trigger.Counts(),
+            steps.Trigger.Counts(useCache = True),
             ]
 
     def listOfSampleDictionaries(self) :
-        return [samples.jetmet, samples.photon, samples.muon]
+        return [samples.HT.ht]
 
     def listOfSamples(self,params) :
         from samples import specify
         out = []
 
-        #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Rob3_skim")
-        #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Rob4_skim")
-        #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.Darren1_skim")
-        
-        #out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Darren1")
-        #out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Darren2")
-        #out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Darren3")
-        out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Darren4")
-        out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Darren5")
-        out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Darren6")
+        out += specify(names = "HT.Run2011A-May10ReReco-v1.AOD.Darren1", )
+        out += specify(names = "HT.Run2011A-05Aug2011-v1.AOD.Bryn1",     )
+        out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Bryn1",    )
+        out += specify(names = "HT.Run2011A-PromptReco-v6.AOD.Bryn1",    )
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn1",    )
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn2",    )
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn3",    )
+
         return out
 
     def conclude(self, conf) :
