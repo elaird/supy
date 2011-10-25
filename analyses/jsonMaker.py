de26d4053de115128b0ee514d569525ac1c593fc
Edward Laird <elaird@gmail.com>
2011-10-25 16:43:12 +0100

    new HT samples

--- analyses/jsonMaker.py
+++ analyses/jsonMaker.py
@@ -12,19 +12,22 @@ class jsonMaker(analysis) :
 
     def listOfSamples(self,params) :
         from samples import specify
-        jwPrompt = calculables.Other.jsonWeight("cert/Cert_160404-177515_7TeV_PromptReco_Collisions11_JSON.txt")
+        jwPrompt = calculables.Other.jsonWeight("cert/Cert_160404-178677_7TeV_PromptReco_Collisions11_JSON.txt")
         jwMay = calculables.Other.jsonWeight("cert/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt")
-        jwAug = calculables.Other.jsonWeight("cert/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v2.txt")
+        jwAug = calculables.Other.jsonWeight("cert/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt")
 
         out = []
 
-        out += specify(names = "HT.Run2011A-May10ReReco-v1.AOD.Darren1", weights = jwMay)
-        out += specify(names = "HT.Run2011A-05Aug2011-v1.AOD.Bryn1",     weights = jwAug)
-        out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Bryn1",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011A-PromptReco-v6.AOD.Bryn1",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn1",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn2",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn3",    weights = jwPrompt)
+        out += specify(names = "HT.Run2011A-May10ReReco-v1.AOD.job536", weights = jwMay)
+        out += specify(names = "HT.Run2011A-05Aug2011-v1.AOD.job528",   weights = jwAug)
+        out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.job535",  weights = jwPrompt)
+        out += specify(names = "HT.Run2011A-PromptReco-v6.AOD.job527",  weights = jwPrompt)
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.job515",  weights = jwPrompt)
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.job519",  weights = jwPrompt)
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.job531",  weights = jwPrompt)
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.job533",  weights = jwPrompt)
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.job564",  weights = jwPrompt)
+        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.job592",  weights = jwPrompt)
 
         #out += specify(names = [ "SingleMu.Run2011A-May10-v1.FJ.Burt",
         #                         "SingleMu.Run2011A-PR-v4.FJ.Burt1",
