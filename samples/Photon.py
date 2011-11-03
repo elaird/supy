f1586ab38ec23d3731303fad9838f7be63512b63
Edward Laird <elaird@gmail.com>
2011-11-03 15:48:06 +0000

    change sample names and add two samples

--- samples/Photon.py
+++ samples/Photon.py
@@ -3,22 +3,15 @@ from core.configuration import srm
 photon = samples.SampleHolder()
 
 #L1FJL2L3Residual
-photon.add("Photon.Run2011A-May10ReReco-v1.AOD.Darren1", '%s/dburton/ICF/automated/2011_10_04_23_05_48/Photon.Run2011A-May10ReReco-v1.AOD")'%srm,
-           lumi = 1.0) #job 536, 1393/1414 completed
-photon.add("Photon.Run2011A-05Aug2011-v1.AOD.Bryn1",     '%s/bm409/ICF/automated/2011_09_29_15_37_16/Photon.Run2011A-05Aug2011-v1.AOD")'%srm,
-           lumi = 1.0) #job 528,  414/ 470 completed
-photon.add("Photon.Run2011A-PromptReco-v4.AOD.Bryn1",    '%s/bm409/ICF/automated/2011_10_04_17_23_30/Photon.Run2011A-PromptReco-v4.AOD")'%srm,
-           lumi = 1.0) #job 535, 1018/1636 completed
-photon.add("Photon.Run2011A-PromptReco-v6.AOD.Bryn1",    '%s/bm409/ICF/automated/2011_09_29_13_50_58/Photon.Run2011A-PromptReco-v6.AOD", alwaysUseLastAttempt = True)'%srm,
-           lumi = 1.0) #job 527,  384/ 647 completed
-photon.add("Photon.Run2011B-PromptReco-v1.AOD.Bryn1",    '%s/bm409/ICF/automated/2011_09_19_19_13_32/Photon.Run2011B-PromptReco-v1.AOD")'%srm,
-           lumi = 1.0) #job 515,  228/ 250 completed
-photon.add("Photon.Run2011B-PromptReco-v1.AOD.Bryn2",    '%s/bm409/ICF/automated/2011_09_26_16_02_44/Photon.Run2011B-PromptReco-v1.AOD")'%srm,
-           lumi = 1.0) #job 519,  259/ 260 completed
-photon.add("Photon.Run2011B-PromptReco-v1.AOD.Bryn3",    '%s/bm409/ICF/automated/2011_10_03_12_23_10/Photon.Run2011B-PromptReco-v1.AOD")'%srm,
-           lumi = 1.0) #job 531,  313/ 338 completed
-#photon.add("Photon.Run2011B-PromptReco-v1.AOD.Bryn4",    '%s/bm409//ICF/automated/2011_10_17_12_55_58/Photon.Run2011B-PromptReco-v1.AOD")'%srm,
-#           lumi = 1.0) #job 570,   82/ 432 completed
+photon.add("Photon.Run2011A-May10ReReco-v1.AOD.job536", '%s/dburton/ICF/automated/2011_10_04_23_05_48/Photon.Run2011A-May10ReReco-v1.AOD")'%srm, lumi = 1.0) #job 536, 1393/1414 completed
+photon.add("Photon.Run2011A-05Aug2011-v1.AOD.job528",   '%s/bm409/ICF/automated/2011_09_29_15_37_16/Photon.Run2011A-05Aug2011-v1.AOD")'%srm, lumi = 1.0) #job 528,  414/ 470 completed
+photon.add("Photon.Run2011A-PromptReco-v4.AOD.job535",  '%s/bm409/ICF/automated/2011_10_04_17_23_30/Photon.Run2011A-PromptReco-v4.AOD")'%srm, lumi = 1.0) #job 535, 1018/1636 completed
+photon.add("Photon.Run2011A-PromptReco-v6.AOD.job527",  '%s/bm409/ICF/automated/2011_09_29_13_50_58/Photon.Run2011A-PromptReco-v6.AOD", alwaysUseLastAttempt = True)'%srm, lumi = 1.0) #job 527,  384/ 647 completed
+photon.add("Photon.Run2011B-PromptReco-v1.AOD.job515",  '%s/bm409/ICF/automated/2011_09_19_19_13_32/Photon.Run2011B-PromptReco-v1.AOD")'%srm, lumi = 1.0) #job 515,  228/ 250 completed
+photon.add("Photon.Run2011B-PromptReco-v1.AOD.job519",  '%s/bm409/ICF/automated/2011_09_26_16_02_44/Photon.Run2011B-PromptReco-v1.AOD")'%srm, lumi = 1.0) #job 519,  259/ 260 completed
+photon.add("Photon.Run2011B-PromptReco-v1.AOD.job531",  '%s/bm409/ICF/automated/2011_10_03_12_23_10/Photon.Run2011B-PromptReco-v1.AOD")'%srm, lumi = 1.0) #job 531,  313/ 338 completed
+photon.add("Photon.Run2011B-PromptReco-v1.AOD.job570",  '%s/bm409//ICF/automated/2011_10_17_12_55_58")'%srm, lumi = 1.0) #job 570,  82/ 432 completed
+photon.add("Photon.Run2011B-PromptReco-v1.AOD.job598",  '%s/bm409//ICF/automated/2011_10_23_13_08_49")'%srm, lumi = 1.0) # 123/ 143 completed
 
 ### EPS below ###
 
