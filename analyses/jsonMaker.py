2a0c6f514614c5687cfb28d604ba19263c2c7e4a
Edward Laird <elaird@gmail.com>
2011-11-03 15:48:29 +0000

    warn about old json and use photon samples

--- analyses/jsonMaker.py
+++ analyses/jsonMaker.py
@@ -4,7 +4,7 @@ import os,steps,samples,calculables
 class jsonMaker(analysis) :
     def listOfSteps(self,params) :
         return [ steps.Print.progressPrinter(2,300),
-                 steps.Other.jsonMaker(),
+                 steps.Other.jsonMaker(calculateLumi = False),
                  ]
 
     def listOfCalculables(self,params) :
@@ -15,16 +15,27 @@ class jsonMaker(analysis) :
         jwPrompt = calculables.Other.jsonWeight("cert/Cert_160404-177515_7TeV_PromptReco_Collisions11_JSON.txt")
         jwMay = calculables.Other.jsonWeight("cert/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt")
         jwAug = calculables.Other.jsonWeight("cert/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v2.txt")
+        print "update Aug5 json to v3"
 
         out = []
 
-        out += specify(names = "HT.Run2011A-May10ReReco-v1.AOD.Darren1", weights = jwMay)
-        out += specify(names = "HT.Run2011A-05Aug2011-v1.AOD.Bryn1",     weights = jwAug)
-        out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Bryn1",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011A-PromptReco-v6.AOD.Bryn1",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn1",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn2",    weights = jwPrompt)
-        out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn3",    weights = jwPrompt)
+        #out += specify(names = "HT.Run2011A-May10ReReco-v1.AOD.Darren1", weights = jwMay)
+        #out += specify(names = "HT.Run2011A-05Aug2011-v1.AOD.Bryn1",     weights = jwAug)
+        #out += specify(names = "HT.Run2011A-PromptReco-v4.AOD.Bryn1",    weights = jwPrompt)
+        #out += specify(names = "HT.Run2011A-PromptReco-v6.AOD.Bryn1",    weights = jwPrompt)
+        #out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn1",    weights = jwPrompt)
+        #out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn2",    weights = jwPrompt)
+        #out += specify(names = "HT.Run2011B-PromptReco-v1.AOD.Bryn3",    weights = jwPrompt)
+
+        out += specify(names = "Photon.Run2011A-May10ReReco-v1.AOD.job536")
+        out += specify(names = "Photon.Run2011A-05Aug2011-v1.AOD.job528")
+        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.job535")
+        out += specify(names = "Photon.Run2011A-PromptReco-v6.AOD.job527")
+        out += specify(names = "Photon.Run2011B-PromptReco-v1.AOD.job515")
+        out += specify(names = "Photon.Run2011B-PromptReco-v1.AOD.job519")
+        out += specify(names = "Photon.Run2011B-PromptReco-v1.AOD.job531")
+        out += specify(names = "Photon.Run2011B-PromptReco-v1.AOD.job570")
+        out += specify(names = "Photon.Run2011B-PromptReco-v1.AOD.job598")
 
         #out += specify(names = [ "SingleMu.Run2011A-May10-v1.FJ.Burt",
         #                         "SingleMu.Run2011A-PR-v4.FJ.Burt1",
