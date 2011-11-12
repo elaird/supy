603d5bbbdaf2003f9d7e3beab6e6131842732362
Burton Betchart <betchart@gmail.com>
2011-11-12 05:51:22 +0000

    topPrinter

--- steps/Gen.py
+++ steps/Gen.py
@@ -1,4 +1,4 @@
-import collections, ROOT as r
+import collections, itertools, ROOT as r
 from core import utils
 from core.analysisStep import analysisStep
 #####################################
@@ -257,6 +257,37 @@ class genParticlePrinter(analysisStep) :
         #outString+="  %#5.1f"%self.sumP4.phi()
         #print outString
         print
+#####################################
+#####################################
+class topPrinter(analysisStep) :
+
+    def uponAcceptance (self,ev) :
+        ids = list(ev['genPdgId'])
+        if not (6 in ids and -6 in ids) : return
+        iTop = ids.index(6)
+        iTbar= ids.index(-6)
+        iQs = range(2,min(iTop,iTbar)) # interested in stuff listed between protons and tops
+        mom = ev['genMotherIndex']
+        p4s = ev['genP4']
+        labels = {1:' d', -1:r'/d', 2:' u', -2:r'/u', 3:' s', -3:r'/s', 4:' c', -4:r'/c', 5:' b', -5:r'/b', 21:' g', 6:' t', -6:'/t'}
+
+        iGs = filter(lambda i: ids[i]==21, range(max(iTop,iTbar)+1,len(ids)))
+
+        print '-'*50
+        print '\t'.join(['item','mom','pt','m','eta','phi'])
+        print
+        for i in iQs+[iTop,iTbar]+iGs :
+            print '\t'.join(str(d)[:5] for d in ["%d(%s)"%(i,labels[ids[i]]), mom[i], p4s[i].pt(), '-', p4s[i].eta(), p4s[i].phi()])
+        print
+        for i,j in itertools.combinations(iQs,2) :
+            if ids[i]*ids[j]>0 : continue
+            p4 = p4s[i]+p4s[j]
+            print '\t'.join(str(d)[:5] for d in ["[%s,%s]"%(i,j), '-', p4.pt(), p4.M(), p4.eta(), p4.phi()])
+        tt = p4s[iTop] + p4s[iTbar]
+        print '\t'.join(str(d)[:5] for d in ["ttbar", '-', tt.pt(), tt.M(), tt.eta(), tt.phi()])
+        print
+        print
+        if abs(tt.E() - (p4s[4]+p4s[5]).E())>0.5 : print (50*' '), "2 -> 3+"
 #####################################
 class genMassHistogrammer(analysisStep) :
 
