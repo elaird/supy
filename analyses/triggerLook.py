1244d878931b12c880b6be9e21674f492bc27909
Edward Laird <elaird@gmail.com>
2011-11-16 16:00:06 +0000

    add samples

--- analyses/triggerLook.py
+++ analyses/triggerLook.py
@@ -14,17 +14,23 @@ class triggerLook(analysis.analysis) :
             ]
 
     def listOfSampleDictionaries(self) :
-        return [samples.HT.ht, samples.Photon.photon]
+        return [samples.HT.ht, samples.Photon.photon, samples.DoubleMu.mumu]
 
     def listOfSamples(self,params) :
         from samples import specify
         out = []
 
-        out += specify(names = "Photon.Run2011A-05Aug2011-v1.AOD.job663")
-        out += specify(names = "Photon.Run2011A-May10ReReco-v1.AOD.job662")
-        out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.job664")
-        out += specify(names = "Photon.Run2011A-PromptReco-v6.AOD.job667")
-        out += specify(names = "Photon.Run2011B-PromptReco-v1.AOD.job668")
+        #out += specify(names = "Photon.Run2011A-05Aug2011-v1.AOD.job663")
+        #out += specify(names = "Photon.Run2011A-May10ReReco-v1.AOD.job662")
+        #out += specify(names = "Photon.Run2011A-PromptReco-v4.AOD.job664")
+        #out += specify(names = "Photon.Run2011A-PromptReco-v6.AOD.job667")
+        #out += specify(names = "Photon.Run2011B-PromptReco-v1.AOD.job668")
+
+        out += specify(names = "DoubleMu.Run2011A-05Aug2011-v1.AOD.job663",  )
+        out += specify(names = "DoubleMu.Run2011A-May10ReReco-v1.AOD.job662",)
+        out += specify(names = "DoubleMu.Run2011A-PromptReco-v4.AOD.job664", )
+        out += specify(names = "DoubleMu.Run2011A-PromptReco-v6.AOD.job665", )
+        out += specify(names = "DoubleMu.Run2011B-PromptReco-v1.AOD.job666", )
 
         return out
 
