b006a3103df27178fbf59ecb8e9754165663645e
Burton Betchart <betchart@gmail.com>
2011-11-19 11:20:43 +0000

    top printer

--- steps/Gen.py
+++ steps/Gen.py
@@ -269,20 +269,25 @@ class topPrinter(analysisStep) :
         iQs = range(2,min(iTop,iTbar)) # interested in stuff listed between protons and tops
         mom = ev['genMotherIndex']
         p4s = ev['genP4']
+        status = ev['genStatus']
         labels = {1:' d', -1:r'/d', 2:' u', -2:r'/u', 3:' s', -3:r'/s', 4:' c', -4:r'/c', 5:' b', -5:r'/b', 21:' g', 6:' t', -6:'/t'}
 
         iGs = filter(lambda i: ids[i]==21, range(max(iTop,iTbar)+1,len(ids)))
+        iDaughters = filter(lambda i : mom[i]<min(iTop,iTbar), range(max(iTop,iTbar)+1,len(ids)))
 
         print '-'*50
         print '\t'.join(['item','mom','pt','m','eta','phi'])
         print
-        for i in iQs+[iTop,iTbar]+iGs :
-            print '\t'.join(str(d)[:5] for d in ["%d(%s)"%(i,labels[ids[i]]), mom[i], p4s[i].pt(), '-', p4s[i].eta(), p4s[i].phi()])
+        for i in iQs+[iTop,iTbar]+iDaughters :
+            print '\t'.join(str(d)[:5] for d in ["%d(%s)"%(i,labels[ids[i]] if ids[i] in labels else str(ids[i])), mom[i], p4s[i].pt(), '-', p4s[i].eta(), p4s[i].phi(), status[i]])
         print
-        for i,j in itertools.combinations(iQs,2) :
-            if ids[i]*ids[j]>0 : continue
-            p4 = p4s[i]+p4s[j]
-            print '\t'.join(str(d)[:5] for d in ["[%s,%s]"%(i,j), '-', p4.pt(), p4.M(), p4.eta(), p4.phi()])
+
+        pD = sum([p4s[i] for i in iDaughters],utils.LorentzV())
+        print '\t'.join(str(d)[:5] for d in ["Daught", '-', pD.pt(), pD.M(), pD.eta(), pD.phi()])
+        p4 = p4s[2]+p4s[3]
+        print '\t'.join(str(d)[:5] for d in ["[%s,%s]"%(2,3), '-', p4.pt(), p4.M(), p4.eta(), p4.phi()])
+        p4 = p4s[4]+p4s[5]
+        print '\t'.join(str(d)[:5] for d in ["[%s,%s]"%(4,5), '-', p4.pt(), p4.M(), p4.eta(), p4.phi()])
         tt = p4s[iTop] + p4s[iTbar]
         print '\t'.join(str(d)[:5] for d in ["ttbar", '-', tt.pt(), tt.M(), tt.eta(), tt.phi()])
         print
