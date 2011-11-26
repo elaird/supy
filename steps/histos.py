135d88a9e3533d8d5393c6bd014ee4d82b67560c
Burton Betchart <betchart@gmail.com>
2011-11-26 03:10:23 +0000

    keep histogrammer and iterhistogrammer for now

--- steps/histos.py
+++ steps/histos.py
@@ -85,37 +85,37 @@ class multiplicity(analysisStep) :
 
 ## Deprecated
 #####################################
-#class histogrammer(analysisStep) :
-#
-#    def __init__(self,var,N,low,up,title="", funcString = "lambda x:x" , suffix = "") :
-#        for item in ["var","N","low","up","title","funcString"] : setattr(self,item,eval(item))
-#        self.oneD = type(var) != tuple
-#        self.hName = (var if self.oneD else "_vs_".join(reversed(var)))+suffix
-#        self.moreName = "%s(%s)"% ("(%s)"%funcString if funcString!="lambda x:x" else "", str(self.hName))
-#        self.funcStringEvaluated = False
-#
-#    def uponAcceptance(self,eventVars) :
-#        if not self.funcStringEvaluated :
-#            self.func = eval(self.funcString)
-#            self.funcStringEvaluated = True
-#
-#        value = eventVars[self.var] if self.oneD else \
-#                tuple(map(eventVars.__getitem__,self.var))
-#        if value is None or (not self.oneD and not all(value)) : return #temporary bug
-#
-#        self.book.fill( self.func(value), self.hName, self.N, self.low, self.up, title=self.title)
-######################################
-#class iterHistogrammer(histogrammer) :
-#
-#    def uponAcceptance(self,eventVars) :
-#        if not self.funcStringEvaluated :
-#            self.func = eval(self.funcString)
-#            self.funcStringEvaluated = True
-#
-#        values = eventVars[self.var] if self.oneD else \
-#                 zip(*map(eventVars.__getitem__,self.var))
-#
-#        for i in range(len(values)) :
-#            value = values[i]
-#            if value is None or (not self.oneD and None in value) : continue
-#            self.book.fill( self.func(value), self.hName, self.N, self.low, self.up, title=self.title)
+class histogrammer(analysisStep) :
+
+    def __init__(self,var,N,low,up,title="", funcString = "lambda x:x" , suffix = "") :
+        for item in ["var","N","low","up","title","funcString"] : setattr(self,item,eval(item))
+        self.oneD = type(var) != tuple
+        self.hName = (var if self.oneD else "_vs_".join(reversed(var)))+suffix
+        self.moreName = "%s(%s)"% ("(%s)"%funcString if funcString!="lambda x:x" else "", str(self.hName))
+        self.funcStringEvaluated = False
+
+    def uponAcceptance(self,eventVars) :
+        if not self.funcStringEvaluated :
+            self.func = eval(self.funcString)
+            self.funcStringEvaluated = True
+
+        value = eventVars[self.var] if self.oneD else \
+                tuple(map(eventVars.__getitem__,self.var))
+        if value is None or (not self.oneD and not all(value)) : return #temporary bug
+
+        self.book.fill( self.func(value), self.hName, self.N, self.low, self.up, title=self.title)
+#####################################
+class iterHistogrammer(histogrammer) :
+
+    def uponAcceptance(self,eventVars) :
+        if not self.funcStringEvaluated :
+            self.func = eval(self.funcString)
+            self.funcStringEvaluated = True
+
+        values = eventVars[self.var] if self.oneD else \
+                 zip(*map(eventVars.__getitem__,self.var))
+
+        for i in range(len(values)) :
+            value = values[i]
+            if value is None or (not self.oneD and None in value) : continue
+            self.book.fill( self.func(value), self.hName, self.N, self.low, self.up, title=self.title)
