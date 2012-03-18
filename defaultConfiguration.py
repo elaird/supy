3d4a416f0a9691e9025204050a495ff311099a6a
Edward Laird <elaird@gmail.com>
2012-03-18 23:38:18 +0000

    change variable name

--- defaultConfiguration.py
+++ defaultConfiguration.py
@@ -40,12 +40,12 @@ def hadd() :
 def dictionariesToGenerate() :
     return []
 
-def initializeROOT(r, sourceFiles = []) :
+def initializeROOT(r, cppFiles = []) :
     r.gROOT.SetStyle("Plain")
     r.gStyle.SetPalette(1)
     r.TH1.SetDefaultSumw2(True)
     r.gErrorIgnoreLevel = 2000
     r.gROOT.SetBatch(True)
 
-    for sourceFile in sourceFiles :
+    for sourceFile in cppFiles :
         r.gROOT.LoadMacro(sourceFile+"+")
