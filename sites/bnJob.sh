4bd5a4e42507b590817170d6afee39719d8bdf5f
Edward Laird <elaird@gmail.com>
2012-03-26 16:05:53 -0400

    start job script

---
+++ sites/bnJob.sh
@@ -0,0 +1,6 @@
+export VO_CMS_SW_DIR=/sharesoft/cmssw
+export SCRAM_ARCH=slc5_amd64_gcc434
+. $VO_CMS_SW_DIR/cmsset_default.sh
+. /sharesoft2/osg/ce/setup.sh
+
+cd /sharesoft/cmssw/slc5_amd64_gcc434/cms/cmssw-patch/CMSSW_4_2_8_patch7 && eval `scram runtime -sh` && cd - >& /dev/null
