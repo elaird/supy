53ee84724894cfcf4073e344fccbeae9afcefc26
gerbaudo <davide.gerbaudo@gmail.com>
2012-04-09 22:41:47 +0200

    cern*.sh : initial import

---
+++ sites/cernJob.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is very supy-d3pdtrig-specific
+# Maybe we can allow the used to specify this script outside of supy?
+
+
+
+export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup
+cd /afs/cern.ch/work/g/gerbaudo/public/trigger/MyRootCoreDir/
+source $AtlasSetup/scripts/asetup.sh AtlasP1HLT,17.1.4.5,here
+source RootCore/scripts/setup.sh
+export STAGE_SVCCLASS=atlcal
+cd -
