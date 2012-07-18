cef0760184b12e6e0d7566288c207dc2a4268b55
Edward Laird <elaird@gmail.com>
2012-07-18 16:35:20 +0200

    toggle setBranchAddress; asserts in concludeAll()

--- tests/01_integers/integers.py
+++ tests/01_integers/integers.py
@@ -1,15 +1,25 @@
 import supy
 
+def dump(orgs = []) :
+    out = "\n"
+    for org in orgs :
+        out += org.tag+": "+str(org)+"\n"
+        for s in org.steps :
+            out += " ".join([s.name,s.title,"\n"])
+        out += "\n"
+    return out
+
 class integers(supy.analysis) :
+    def parameters(self) :
+        return {"setBranchAddress": self.vary({"sba":True, "no-sba":False}), }
     
     def mainTree(self) :
         return ("djtuple", "tree")
 
     def listOfSteps(self,config) :
-        return [
-            supy.steps.printer.progressPrinter(),
-            supy.steps.histos.value('njets',18,-2,15),
-            ]
+        return [supy.steps.printer.progressPrinter(),
+                supy.steps.histos.value('njets',18,-2,15),
+                ] + [supy.steps.other.noSetBranchAddress()] if not config["setBranchAddress"] else []
     
     def listOfCalculables(self,config) :
         return supy.calculables.zeroArgs(supy.calculables)
@@ -22,6 +32,13 @@ class integers(supy.analysis) :
     def listOfSamples(self,config) :
         return supy.samples.specify(names = "integers")
 
-options = supy.tests.defaultOptions()
-options.loop = 1
-integers(options).loop()
+    def concludeAll(self) :
+        orgs = map(lambda x:self.organizer(x), self.readyConfs)
+        fail = dump(orgs)
+        assert len(orgs)==2,fail
+
+        lst = [len(org.steps) for org in orgs]
+        assert max(lst)==4,fail
+        assert min(lst)==3,fail
+
+supy.tests.run(analysis = integers, options = {"loop": 1})
