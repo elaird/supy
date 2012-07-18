b2d1a64e184afb19304fe1f63666769644e97636
Edward Laird <elaird@gmail.com>
2012-07-18 15:26:23 +0200

    add run sequence

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,3 +16,21 @@ class defaultOptions(object) :
     samples = None
     omit = ""
     nocheck = False
+
+
+def run(analysis = None, options = {}) :
+    "see supy/bin/supy"
+
+    opts = defaultOptions()
+    for key,value in options.iteritems() :
+        setattr(opts, key, value)
+
+    assert opts.jobId==None
+    assert not opts.batch
+
+    a = analysis(opts)
+    a.loop()
+    a.mergeAllOutput()
+    a.manageSecondaries(opts.update, opts.report)
+    if opts.update==None and opts.report==None :
+        a.concludeAll()
