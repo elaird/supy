d62a7101bf383d26a75106d3066ce969a7305ffd
Edward Laird <elaird@gmail.com>
2012-07-18 15:15:04 +0200

    stash default options

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+class defaultOptions(object) :
+    "in normal usage, these are given to supy as options"
+
+    #defaults
+    loop = None
+    slices = None
+    profile = False
+    batch = False
+    tag = ""
+    sample = None
+    update = None
+    report = None
+    jobId = None
+    site = None
+    tags = None
+    samples = None
+    omit = ""
+    nocheck = False
