f20aee5a15b415c07d06e25accf2760a206944b8
Burton Betchart <betchart@gmail.com>
2012-07-20 01:07:24 +0100

    module rollback

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,14 @@
-import unittest
+import unittest,sys
+
+class temporaryImports(set) :
+    '''All modules imported under "with" expression are unloaded upon exit.'''
+    def __enter__(self) : self.update(sys.modules)
+    def __exit__(self,type,value,traceback) :
+        for modname in set(sys.modules)-self : del sys.modules[modname]
+
 
 if __name__ == "__main__" :
     for mod in ["integers"] :
-        suite = unittest.TestLoader().loadTestsFromName(mod)
-        unittest.TextTestRunner(verbosity=2).run(suite)
+        with temporaryImports() as _ :
+            suite = unittest.TestLoader().loadTestsFromName(mod)
+            unittest.TextTestRunner(verbosity=2).run(suite)
