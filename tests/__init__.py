ba36fdb4aaad5b5f1082953ce22b705182e49dcd
Burton Betchart <betchart@gmail.com>
2012-07-23 13:33:58 -0400

    less verbose tests by default.

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,7 +20,7 @@ if __name__ == "__main__" :
             msg = "|| %s ||"%mod
             print msg.join(2*'\n').join( 2 * [len(msg)*"="] )
             suite = unittest.TestLoader().loadTestsFromName(mod)
-            unittest.TextTestRunner(verbosity=2).run(suite)
+            unittest.TextTestRunner(verbosity=1).run(suite)
             sys.path = sys.path[:1] + sys.path[2:]
             print
         print
