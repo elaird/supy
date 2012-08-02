ae4cc985f7e875ba1c1c59045e6f1f1369737f3f
Burton Betchart <betchart@gmail.com>
2012-08-02 22:01:47 +0100

    tests/asymmweight

--- tests/asymmweight/__init__.py
+++ tests/asymmweight/__init__.py
@@ -1,4 +1,4 @@
-import unittest,supy,configuration,os,itertools,ROOT as r
+import math,unittest,supy,configuration,os,itertools,ROOT as r
 from supy.tests import expectedFailure,skipIf
 try: import numpy as np
 except: np=None
@@ -35,8 +35,48 @@ class reindex(unittest.TestCase) :
                 self.assertEqual( y[i[j],i[k]], Y[j,k])
             
 
-class qqbar_hard(unittest.TestCase) :
-    def setUp(self) : pass
-    @expectedFailure
+class asymm_hard(unittest.TestCase) :
+    @skipIf(np==None,"System lacks numpy")
     def test(self) :
-        self.fail("not yet implemented")
+        '''supy.utils.asymmWeighting.Asymm_hard'''
+        import ROOT as r
+        from supy.utils.asymmWeighting import Asymm_qqbar_hard
+
+        LV = r.Math.LorentzVector(r.Math.PtEtaPhiM4D('double'))
+        q = LV(18.2,3.9,1.0,0)
+        qbar = LV(5.0,-4.3,1.9,0)
+        Q = LV(160.9,1.0,0.3,174.0)
+        Qbar = LV(103.2,0.8,2.4,174.1)
+        g = LV(120.7,0.1,-2.1,0)
+        qqbarQQbarg = [q,qbar,Q,Qbar,g]
+
+        asym = Asymm_qqbar_hard()
+        asym.setMomenta(qqbarQQbarg)
+        symm,anti = asym.symm,asym.anti
+        asym.setMomenta([qbar,q,Q,Qbar,g])
+        self.assertAlmostEqual(symm, asym.symm)
+        self.assertAlmostEqual(anti, -asym.anti)
+        asym.setMomenta([qbar,q,Qbar,Q,g])
+        self.assertAlmostEqual(symm, asym.symm)
+        self.assertAlmostEqual(anti, asym.anti)
+        asym.setMomenta([q,qbar,Qbar,Q,g])
+        self.assertAlmostEqual(symm, asym.symm)
+        self.assertAlmostEqual(anti, -asym.anti)
+
+    def testConstant(self) :
+        self.assertEqual(1.1**3 *10 / (432*math.pi),
+                         supy.utils.asymmWeighting.Asymm_hard.constant())
+
+    @skipIf(np==None,"System lacks numpy")
+    def testKernelQQbar(self) :
+        '''supy.utils.asymmWeighting.Asymm_qqbar_hard.kernel'''
+        Y = np.ones(25).reshape((5,5))
+        k = supy.utils.asymmWeighting.Asymm_qqbar_hard.kernel(Y,1)
+        self.assertEqual(k, 9./2)
+
+    @skipIf(np==None,"System lacks numpy")
+    def testKernelQg(self) :
+        '''supy.utils.asymmWeighting.Asymm_qg_hard.kernel'''
+        Y = np.ones(25).reshape((5,5))
+        k = supy.utils.asymmWeighting.Asymm_qg_hard.kernel(Y,1)
+        self.assertEqual(k, 2)
