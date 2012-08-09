6eea4a253ce70b824359fd7ad1b1ece0cbf514f2
gerbaudo <davide.gerbaudo@gmail.com>
2012-08-09 14:57:33 +0200

    psFileName -> pdfFileName

--- examples/example_analysis.py
+++ examples/example_analysis.py
@@ -30,7 +30,7 @@ class example_analysis(supy.analysis) :
         org = self.organizer(pars)
         org.scale()
         supy.plotter( org,
-                      psFileName = self.psFileName(org.tag),
+                      pdfFileName = self.pdfFileName(org.tag),
                       samplesForRatios = ("Example_Skimmed_900_GeV_Data","Example_Skimmed_900_GeV_MC"),
                       sampleLabelsForRatios = ("data","sim"),
                       ).plotAll()
