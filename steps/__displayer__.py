606229aaec5e24408ea44496bc03e4c69331118f
Edward Laird <elaird@gmail.com>
2013-03-28 04:28:50 -0500

    pep8

--- steps/__displayer__.py
+++ steps/__displayer__.py
@@ -1,64 +1,71 @@
-import os, ROOT as r
-from supy import utils,analysisStep
+import os
+import ROOT as r
+import supy
 
-class displayer(analysisStep) :
 
-    def reset(self) :
+class displayer(supy.analysisStep):
+    def reset(self):
         self.canvas.Clear()
-    
-    def display(self, eventVars) :
+
+    def display(self, eventVars):
         """Implement drawing etc. in self.display.
-        It may be necessary to return ROOT objects to prevent them from getting garbage-collected.
+        It may be necessary to return ROOT objects
+        to prevent them from getting garbage-collected.
         """
         return locals()
 
-    def uponAcceptance(self, eventVars) :
+    def uponAcceptance(self, eventVars):
         self.reset()
         stuff = self.display(eventVars)
         self.writeCanvas()
 
-    def writeCanvas(self) :
+    def writeCanvas(self):
         someDir = r.gDirectory
         self.outputFile.cd()
-        self.canvas.Write("canvas_%d"%self.canvasIndex)
-        self.canvasIndex+=1
+        self.canvas.Write("canvas_%d" % self.canvasIndex)
+        self.canvasIndex += 1
         someDir.cd()
 
-    def setup(self, chain, fileDir) :
+    def setup(self, chain, fileDir):
         someDir = r.gDirectory
         self.outputFile = r.TFile(self.outputFileName, "RECREATE")
         someDir.cd()
 
-        self.canvas = utils.canvas("canvas")
+        self.canvas = supy.utils.canvas("canvas")
         self.canvas.SetFixedAspectRatio()
         self.canvasIndex = 0
 
-    def endFunc(self, chains) :
+    def endFunc(self, chains):
         self.reset()
         self.outputFile.Write()
         self.outputFile.Close()
         del self.canvas
 
-    def outputSuffix(self) :
+    def outputSuffix(self):
         return "_displays.root"
 
-    def mergeFunc(self, products) :
-        def pdfFromRoot(listOfInFileNames, outFileName) :
-            if not len(listOfInFileNames) : return
+    def mergeFunc(self, products):
+        def pdfFromRoot(listOfInFileNames, outFileName):
+            if not len(listOfInFileNames):
+                return
             options = "pdf"
-            dummyCanvas = utils.canvas("display")
+            dummyCanvas = supy.utils.canvas("display")
             dummyCanvas.Print(outFileName+"[", options)
-            for inFileName in listOfInFileNames :
+            for inFileName in listOfInFileNames:
                 inFile = r.TFile(inFileName)
                 keys = inFile.GetListOfKeys()
-                for key in keys :
+                for key in keys:
                     someObject = inFile.Get(key.GetName())
-                    if someObject.ClassName()!="TCanvas" : print "Warning: found an object which is not a TCanvas in the display root file"
+                    if someObject.ClassName() != "TCanvas":
+                        print "Warning: found an non-TCanvas object" + \
+                              " in the display root file"
                     someObject.Print(outFileName, options)
                 inFile.Close()
-                os.remove(inFileName)                    
+                os.remove(inFileName)
             dummyCanvas.Print(outFileName+"]", options)
-            print "The display file \""+outFileName+"\" has been written."    
-        
-        pdfFromRoot(products["outputFileName"], self.outputFileName.replace(".root", ".pdf"))
-        print utils.hyphens
+            print "The display file \""+outFileName+"\" has been written."
+
+        pdfFromRoot(products["outputFileName"],
+                    self.outputFileName.replace(".root", ".pdf"),
+                    )
+        print supy.utils.hyphens
