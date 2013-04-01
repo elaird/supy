a2a261fcfd1c92ca69f0fa01ac59e1be73f22fdc
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-31 21:54:30 -0700

    add librariesToLoad

--- tests/wrapobject/configuration.py
+++ tests/wrapobject/configuration.py
@@ -1,6 +1,18 @@
+import supy
 from supy.defaults import *
 
 def localpath() :
     return "tests/wrapobject"
 def ttreecacheMB() :
     return 20
+
+def librariesToLoad() :
+    return ['libPhysics.so',
+            "%s/cpp/libSusy.so"%supy.whereami()
+            ]
+
+def initializeROOT(r, cppFiles = []) :
+    supy.defaults.initializeROOT(r, cppFiles)
+    for l in librariesToLoad() :
+        r.gSystem.Load(l)
+
