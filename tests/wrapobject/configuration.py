2dc4fc34663762cd1c5f026b9481f1acb6e6c358
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-31 21:40:15 -0700

    move to a separate test dir

---
+++ tests/wrapobject/configuration.py
@@ -0,0 +1,6 @@
+from supy.defaults import *
+
+def localpath() :
+    return "tests/wrapobject"
+def ttreecacheMB() :
+    return 20
---
+++ tests/wrapobject/example_analysis.py
@@ -0,0 +1,29 @@
+import supy, ROOT as r
+
+class example_analysis(supy.analysis) :
+    
+    def listOfSteps(self,config) :
+        return [
+            supy.steps.printer.progressPrinter(),
+            ]
+    
+    def listOfCalculables(self,config) :
+        return ( supy.calculables.zeroArgs(supy.calculables) +
+                 [supy.calculables.other.fixedValue('Two',2) ]
+                 )
+    
+    def listOfSampleDictionaries(self) :
+        exampleDict = supy.samples.SampleHolder()
+        exampleDict.add("Example_Data", '["/tmp/supy-dev/supy/tests/wrappedchain/nonflat_typetree.root"]', lumi = 1.0e-5 ) #/pb
+        return [exampleDict]
+    
+    def listOfSamples(self,config) :
+        return (supy.samples.specify(names = "Example_Data", color = r.kBlack, markerStyle = 20))
+    
+    def conclude(self,pars) :
+        #make a pdf file with plots from the histograms created above
+        org = self.organizer(pars)
+        org.scale()
+        supy.plotter( org,
+                      pdfFileName = self.pdfFileName(org.tag),
+                      ).plotAll()
