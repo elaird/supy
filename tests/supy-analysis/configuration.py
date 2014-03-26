e332ce3ffe2fce371345d2a154731424b2f4654c
Edward Laird <elaird@gmail.com>
2014-03-26 06:32:04 -0500

    add an aux tree

--- tests/supy-analysis/configuration.py
+++ tests/supy-analysis/configuration.py
@@ -3,6 +3,9 @@ from supy.defaults import *
 def mainTree() :
     return ("djtuple","tree")
 
+def otherTreesToKeepWhenSkimming() :
+    return [("otherTree","tree")]
+
 def nCoresDefault() :
     return 1
 
