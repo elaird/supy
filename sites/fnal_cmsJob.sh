a9c51cf3f67f21a69fc000b890d5223cab04b971
Yossof Eshaq <yossofeshaq@gmail.com>
2014-06-12 10:51:47 -0500

    source different CMSSW depending on host

--- sites/fnal_cmsJob.sh
+++ sites/fnal_cmsJob.sh
@@ -3,4 +3,9 @@
 source /uscmst1/prod/sw/cms/shrc prod
 export SCRAM_ARCH=slc5_amd64_gcc462
 #cd /uscmst1/prod/sw/cms/slc5_amd64_gcc462/cms/cmssw-patch/CMSSW_5_2_4_patch1/src && eval \`scram runtime -sh\` && cd - >& /dev/null
-cd /uscms/home/elaird/CMSSW_5_2_5/src && eval `scram runtime -sh` && cd - >& /dev/null
+
+if [[ "$HOSTNAME" == *cmslpc*.fnal.gov ]]; then
+    cd /uscms/home/elaird/CMSSW_5_2_5/src && eval `scram runtime -sh` && cd - >& /dev/null
+else
+    cd /uscmst1/prod/sw/cms/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_2_5/src && eval \`scram runtime -sh\` && cd - >& /dev/null
+fi
