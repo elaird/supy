90a05f9c341a070087e9bf52736245151f30a9fe
Edward Laird <elaird@gmail.com>
2015-01-23 03:48:13 -0600

    tweak

--- sites/fnal_cmsJob.sh
+++ sites/fnal_cmsJob.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-source /uscmst1/prod/sw/cms/shrc prod
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc5_amd64_gcc462
 
-cd /cvmfs/cms.cern.ch/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_2_5/src && eval \`scram runtime -sh\` && cd - >& /dev/null
+cd /cvmfs/cms.cern.ch/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_2_5/src && eval `scram runtime -sh` && cd - >& /dev/null
 
 #INSERT_BATCH_SETUP
 
