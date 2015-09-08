d52c8b871acda5d2dace97f3b3c9f87601489ba8
Edward Laird <elaird@gmail.com>
2015-09-08 11:10:01 -0400

    find scram

--- sites/bn_cmsJob.sh
+++ sites/bn_cmsJob.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SCRAM_ARCH=slc6_amd64_gcc491
+export PATH=${PATH}:/cvmfs/cms.cern.ch/common
 cd ~elaird/cmssws/CMSSW_7_4_7/src && eval `scram runtime -sh` && cd - >& /dev/null
 
 #INSERT_BATCH_SETUP
