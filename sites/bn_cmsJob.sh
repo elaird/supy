dcda174d1e60877f7cc2cd4ea4eea8df91191dee
Edward Laird <elaird@gmail.com>
2015-09-08 10:41:15 -0400

    update release

--- sites/bn_cmsJob.sh
+++ sites/bn_cmsJob.sh
@@ -1,8 +1,6 @@
-export VO_CMS_SW_DIR=/sharesoft/cmssw
-export SCRAM_ARCH=slc5_amd64_gcc434
-. $VO_CMS_SW_DIR/cmsset_default.sh
-. /sharesoft2/osg/ce/setup.sh
+#!/bin/bash
 
-cd /sharesoft/cmssw/slc5_amd64_gcc434/cms/cmssw-patch/CMSSW_4_2_8_patch7 && eval `scram runtime -sh` && cd - >& /dev/null
+export SCRAM_ARCH=slc6_amd64_gcc491
+cd ~elaird/cmssws/CMSSW_7_4_7/src && eval `scram runtime -sh` && cd - >& /dev/null
 
 #INSERT_BATCH_SETUP
